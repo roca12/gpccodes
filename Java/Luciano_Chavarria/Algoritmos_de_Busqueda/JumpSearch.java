893ffd0f73a0e0f8b2f7b7228541daf1c69fbbb8
Luciano890 <62488915+Luciano890@users.noreply.github.com>
2020-06-09 11:57:54 -0500

    Create JumpSearch.java

---
+++ Java/Luciano_Chavarria/Algoritmos_de_Busqueda/JumpSearch.java
@@ -0,0 +1,40 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int arr[] = {0, 1, 1, 2, 3, 5, 8, 13, 21,
+            34, 55, 89, 144, 233, 377, 610};
+        int x = 1000;
+
+        int resultado = jumpSearch(arr, x);
+        if (resultado == -1) {
+            System.out.println("no encontrado");
+        } else {
+            System.out.println("Encontrado en el indice " + resultado);
+        }
+
+    }
+
+    static int jumpSearch(int arr[], int x) {
+        int n = arr.length;
+        int step = (int) Math.floor(Math.sqrt(n));
+        int prev = 0;
+        while (arr[Math.min(step, n) - 1] < x) {
+            prev = step;
+            step += (int) Math.floor(Math.sqrt(n));
+            if (prev >= n) {
+                return -1;
+            }
+        }
+        while (arr[prev] < x) {
+            prev++;
+            if (prev == Math.min(step, n)) {
+                return -1;
+            }
+        }
+        if (arr[prev] == x) {
+            return prev;
+        }
+        return -1;
+    }
+
+}
