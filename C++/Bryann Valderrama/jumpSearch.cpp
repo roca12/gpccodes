c830a36a5e9ce0f6591026f27adc2ecfb20b41ae
Diego Rodriguez <44822667+roca12@users.noreply.github.com>
2020-06-10 10:49:42 -0500

    no message

--- C++/Bryann Valderrama/jumpSearch.cpp
+++ C++/Bryann Valderrama/jumpSearch.cpp
@@ -1,49 +1,38 @@
-#include <iostream>
-#include <cmath>
+#include <cstdlib>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int jumpSearch(int lista[], int x)
-{
-    int tamanio = sizeof(lista) / sizeof(lista[0]);
-    int salto = (int)floor(sqrt(tamanio));
+int jumpSearch(int lista[], int x, int n) {
+    int salto = (int) floor(sqrt(n));
     int previo = 0;
-    while (lista[min(salto, tamanio) - 1] < x)
-    {
+    while (lista[min(salto, n) - 1] < x) {
         previo = salto;
-        salto = (int)salto + (int)floor(sqrt(tamanio));
-        if (previo >= tamanio)
-        {
+        salto = (int) salto + (int) floor(sqrt(n));
+        if (previo >= n) {
             return -1;
         }
     }
-    while (lista[previo] < x)
-    {
+    while (lista[previo] < x) {
         previo++;
-        if (previo == min(salto, tamanio))
-        {
+        if (previo == min(salto, n)) {
             return -1;
         }
     }
-    if (lista[previo] == x)
-    {
+    if (lista[previo] == x) {
         return previo;
     }
     return -1;
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     int lista[] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144};
     int numeroabuscar;
     cin >> numeroabuscar;
-    int resultado = jumpSearch(lista, numeroabuscar);
-    if (resultado == -1)
-    {
+    int resultado = jumpSearch(lista, numeroabuscar, sizeof (lista) / sizeof (lista[0]));
+    if (resultado == -1) {
         cout << "No encontrado" << endl;
-    }
-    else
-    {
+    } else {
         cout << "Encontrado en el indice " << resultado << endl;
     }
     return 0;
