745102f327990211934ee25f34021147f0fbeeef
Luciano890 <62488915+Luciano890@users.noreply.github.com>
2020-06-10 10:46:52 -0500

    Create JumpSearch.cpp

---
+++ C++/Luciano_Chavarria/Algortimos_de_Busqueda/JumpSearch.cpp
@@ -0,0 +1,46 @@
+#include<cstdlib>
+#include<bits/stdc++.h>
+using namespace std;
+
+int jumpSearch(int arr[],int x,int n){
+	int step = (int) floor(sqrt(n));
+	int prev = 0;
+	while (arr[min(step, n) - 1] < x) {
+		prev = step;
+		step += (int) floor(sqrt(n));
+		if (prev >= n) {
+			return -1;
+		}
+	}
+	while (arr[prev] < x) {
+		prev++;
+		if (prev == min(step, n)) {
+			return -1;
+		}
+	}
+	if (arr[prev] == x) {
+		return prev;
+	}
+	return -1;
+}
+
+
+
+
+int main (int argc, char *argv[]) {
+	int arr[] = {0, 1, 1, 2, 3, 5, 8, 13, 21,
+		34, 55, 89, 144, 233, 377, 610};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	int x = 144;
+	
+	int resultado = jumpSearch(arr, x,n);
+	if (resultado == -1) {
+		cout<<"no encontrado"<<endl;
+	} else {
+		cout<<"Encontrado en el indice " << resultado<<endl;
+	}
+	
+	return 0;
+}
+
+
