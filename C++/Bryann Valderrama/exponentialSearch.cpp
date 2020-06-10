1caa1cc441d5ae15ba73fda7389e101a8a80df0e
Diego Rodriguez <44822667+roca12@users.noreply.github.com>
2020-06-10 10:22:56 -0500

    no message

--- C++/Bryann Valderrama/exponentialSearch.cpp
+++ C++/Bryann Valderrama/exponentialSearch.cpp
@@ -1,54 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int binarySearch(int arr[], int inicio, int fin, int numeroabuscar)
-{
-    if (fin >= inicio)
-    {
-        int medio = inicio + (fin - 1) / 2;
-        if (arr[medio] == numeroabuscar)
-        {
-            return medio;
+int binarySearch(int arr[], int l, int r, int x) { 
+    if (r >= l) { 
+        int mid = l + (r - l)/2; 
+        if (arr[mid] == x){
+            return mid; 
         }
-        if (arr[medio] > numeroabuscar)
-        {
-            return binarySearch(arr, inicio, medio - 1, numeroabuscar);
-        }
-        else
-        {
-            return binarySearch(arr, medio + 1, fin, numeroabuscar);
-        }
-    }
-    return -1;
+        if (arr[mid] > x) {
+            return binarySearch(arr, l, mid-1, x); 
+        }else{
+        return binarySearch(arr, mid+1, r, x); 
+    	}
+    } 
+
+    return -1; 
 }
 
-static int exponentialSearch(int arr[], int n, int x)
-{
-    if (arr[0] == x)
-    {
+int exponentialSearch(int arr[], int n, int x) {
+    if (arr[0] == x) {
         return 0;
     }
     int i = 1;
-    while (i < n && arr[i] <= x)
-    {
+    while (i < n && arr[i] <= x) {
         i = i * 2;
     }
     return binarySearch(arr, i / 2, min(i, n), x);
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     int arr[] = {1, 2, 3, 4, 5};
-    int tamanio = sizeof(arr) / sizeof(arr[0]);
+    int tamanio = sizeof (arr) / sizeof (arr[0]);
     int numeroabuscar;
     cin >> numeroabuscar;
     int resultado = exponentialSearch(arr, tamanio, numeroabuscar);
-    if (resultado < 0)
-    {
+    if (resultado < 0) {
         cout << "El resultado no esta presente en el Array" << endl;
-    }
-    else
-    {
+    } else {
         cout << "Elemento encontrado en el indice " << resultado << endl;
     }
 
