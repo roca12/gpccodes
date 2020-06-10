43e7ba7acb6168973458da3b5a2f346caeddfcc7
BryanttV <64033729+BryanttV@users.noreply.github.com>
2020-06-10 10:32:35 -0500

    Update exponentialSearch.cpp

--- C++/Bryann Valderrama/exponentialSearch.cpp
+++ C++/Bryann Valderrama/exponentialSearch.cpp
@@ -1,44 +1,55 @@
 #include <iostream>
 using namespace std;
 
-int binarySearch(int arr[], int l, int r, int x) { 
-    if (r >= l) { 
-        int mid = l + (r - l)/2; 
-        if (arr[mid] == x){
-            return mid; 
+int binarySearch(int lista[], int izq, int der, int numeroabuscar)
+{
+    if (der >= izq)
+    {
+        int medio = izq + (der - izq) / 2;
+        if (lista[medio] == numeroabuscar)
+        {
+            return medio;
         }
-        if (arr[mid] > x) {
-            return binarySearch(arr, l, mid-1, x); 
-        }else{
-        return binarySearch(arr, mid+1, r, x); 
-    	}
-    } 
-
-    return -1; 
+        if (lista[medio] > numeroabuscar)
+        {
+            return binarySearch(lista, izq, medio - 1, numeroabuscar);
+        }
+        else
+        {
+            return binarySearch(lista, medio + 1, der, numeroabuscar);
+        }
+    }
+    return -1;
 }
 
-int exponentialSearch(int arr[], int n, int x) {
-    if (arr[0] == x) {
+int exponentialSearch(int lista[], int n, int x)
+{
+    if (lista[0] == x)
+    {
         return 0;
     }
     int i = 1;
-    while (i < n && arr[i] <= x) {
+    while (i < n && lista[i] <= x)
+    {
         i = i * 2;
     }
-    return binarySearch(arr, i / 2, min(i, n), x);
+    return binarySearch(lista, i / 2, min(i, n), x);
 }
 
-int main(int argc, char const *argv[]) {
-    int arr[] = {1, 2, 3, 4, 5};
-    int tamanio = sizeof (arr) / sizeof (arr[0]);
+int main(int argc, char const *argv[])
+{
+    int lista[] = {1, 2, 3, 4, 5, 6, 7};
+    int tamanio = sizeof(lista) / sizeof(lista[0]);
     int numeroabuscar;
     cin >> numeroabuscar;
-    int resultado = exponentialSearch(arr, tamanio, numeroabuscar);
-    if (resultado < 0) {
+    int resultado = exponentialSearch(lista, tamanio, numeroabuscar);
+    if (resultado < 0)
+    {
         cout << "El resultado no esta presente en el Array" << endl;
-    } else {
+    }
+    else
+    {
         cout << "Elemento encontrado en el indice " << resultado << endl;
     }
-
     return 0;
 }
