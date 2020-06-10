5ac8e04e59d0f236c0f9fa0dd39b08bc0d8f5c96
Cristiank17 <62625689+Cristiank17@users.noreply.github.com>
2020-06-10 13:03:31 -0500

    Create Max_Min_Search.java

---
+++ Java/Cristian Maldonado/Max_Min_Search.java
@@ -0,0 +1,39 @@
+public class MaxMinSearch {
+
+	public static void main(String[] args) {
+		int arr[] = { 1000, 11, 445, 1, 330, 3000 };
+		int n = arr.length;
+		Pair minPair = getMinMax(arr, n);
+		System.out.printf("El minimo elemento es %d", minPair.min);
+		System.out.printf("\nEl maximo elemento es %d", minPair.max);
+	}
+
+	static class Pair {
+		int min;
+		int max;
+	}
+
+	static Pair getMinMax(int arr[], int n) {
+		Pair minmax = new Pair();
+		if (n == 1) {
+			minmax.max = arr[0];
+			minmax.min = arr[0];
+		}
+		if (arr[0] > arr[1]) {
+			minmax.max = arr[0];
+			minmax.min = arr[1];
+		} else {
+			minmax.max = arr[1];
+			minmax.min = arr[0];
+		}
+		for (int j = 2; j < n; j++) {
+			if (arr[j] > minmax.max) {
+				minmax.max = arr[j];
+			} else if (arr[j] < minmax.min) {
+				minmax.min = arr[j];
+			}
+		}
+		return minmax;
+	}
+
+}
