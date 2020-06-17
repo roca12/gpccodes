b6b25456181847f85b18d5cafa17d2d12b716af7
Luciano890 <62488915+Luciano890@users.noreply.github.com>
2020-06-17 10:47:32 -0500

    Create QuickSort.java

---
+++ Java/Luciano_Chavarria/Algoritmo_de_Ordenamiento/QuickSort.java
@@ -0,0 +1,45 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String sc[] = br.readLine().split(" ");
+        int arr[] = new int[sc.length];
+        for (int i = 0; i < sc.length; i++) {
+            arr[i] = Integer.parseInt(sc[i]);
+        }
+        QuickSort(arr, 0, arr.length-1);
+        for (int i : arr) {
+            System.out.println(i+" ");
+        }
+    }
+
+    static int Partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+    static void QuickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pi = Partition(arr, low, high);
+            QuickSort(arr, low, pi - 1);
+            QuickSort(arr, pi + 1, high);
+        }
+    }
+
+}
