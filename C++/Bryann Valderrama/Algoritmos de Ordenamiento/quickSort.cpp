b0717e3d3fa2464c370c7335b34bc4a7aac6a6a0
BryanttV <64033729+BryanttV@users.noreply.github.com>
2020-06-19 08:56:30 -0500

    Create quickSort.cpp

---
+++ C++/Bryann Valderrama/Algoritmos de Ordenamiento/quickSort.cpp
@@ -0,0 +1,51 @@
+#include <cstdlib>
+#include <bits/stdc++.h>
+using namespace std;
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = partition(arr, low, high);
+        quickSort(arr, low, pivot - 1);
+        quickSort(arr, pivot + 1, high);
+    }
+}
+
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    int arr[] = {3, 5, 42, -64, 1, 24, 5, 6, 10};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    quickSort(arr, 0, len - 1);
+    print(arr, len);
+    return 0;
+}
