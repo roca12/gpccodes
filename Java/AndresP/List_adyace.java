440164a624997626d1d1985ec2d194c86ac75c5e
andres-rgb <62677451+andres-rgb@users.noreply.github.com>
2020-06-24 11:56:39 -0500

    Create List_adyace.java

---
+++ Java/AndresP/List_adyace.java
@@ -0,0 +1,31 @@
+
+package javaapplication24;
+import java.io.IOException;
+import java.util.Arrays;
+
+import java.util.Scanner;
+
+public class List_adyace {
+   static int G[][];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		    int V,E;
+		    V=sc.nextInt();
+		    E=sc.nextInt();
+		    G=new int[V][V];
+		    for(int i=0;i<E;++i){
+		        int desde,hasta,peso;
+		        desde=sc.nextInt();
+		        hasta=sc.nextInt();
+		        peso=sc.nextInt();
+		        G[desde][hasta]=peso;
+		        G[hasta][desde]=peso;
+		    }
+		    for(int i=0;i<V;++i){
+		        for(int j=0;j<V;++j){
+		        	System.out.print(G[i][j]+"\t");
+		        }
+		        System.out.println();
+		    }
+	}
+}
