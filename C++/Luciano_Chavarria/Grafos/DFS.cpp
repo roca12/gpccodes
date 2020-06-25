821a8d194f167bd91b69dd071529595d63874c33
Luciano890 <62488915+Luciano890@users.noreply.github.com>
2020-06-25 12:09:38 -0500

    Update DFS.cpp

--- C++/Luciano_Chavarria/Grafos/DFS.cpp
+++ C++/Luciano_Chavarria/Grafos/DFS.cpp
@@ -1,38 +1,40 @@
 #include <bits/stdc++.h>
 #define MAX_V 101
+//typedef pair Edge;
 using namespace std;
 vector<int> adj[MAX_V];
-struct Graph{
+
+struct Graph {
     int V;
-    void addEdge(int v,int w){
+    void addEdge(int v, int w) {
         adj[v].push_back(w);
-        }
-    void DFSUtil(int v,bool visited[]){
-        visited[v]=true;
-        cout<<v<<" ";
-        for(int i=0;i<adj[v].size();++i){
-            int n=adj[v][i];
-            if(!visited[n]){
-                DFSUtil(n,visited);
+    }
+    void DFSUtil(int v, bool visited[]) {
+        visited[v] = true;
+        cout << v << " ";
+        for (int i = 0; i < adj[v].size(); ++i) {
+            int n = adj[v][i];
+            if (!visited[n]) {
+                DFSUtil(n, visited);
             }
         }
     }
-     void DFS(int v){
+    void DFS(int v) {
         bool visited[V];
-        DFSUtil(v,visited);
+        DFSUtil(v, visited);
     }
 };
 
-int main()
-{
-    Graph gra={4};
-    gra.addEdge(0,1);
-    gra.addEdge(0,2);
-    gra.addEdge(1,2);
-    gra.addEdge(2,0);
-    gra.addEdge(2,3);
-    gra.addEdge(3,3);
-    int initial=0;
+int main() {
+    Graph gra;
+    gra.V=4;
+    gra.addEdge(0, 1);
+    gra.addEdge(0, 2);
+    gra.addEdge(1, 2);
+    gra.addEdge(2, 0);
+    gra.addEdge(2, 3);
+    gra.addEdge(3, 3);
+    int initial = 0;
     gra.DFS(initial);
     return 0;
 }
