b36a590e15d3e7766143fec8cdeb7bea5a4e4115
Luciano890 <62488915+Luciano890@users.noreply.github.com>
2020-06-25 11:46:00 -0500

    Create Matrixady.cpp

---
+++ C++/Luciano_Chavarria/Grafos/Matrixady.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define MAX_V 101
+using namespace std;
+static int G[MAX_V][MAX_V];
+int main()
+{
+    memset(G,0,sizeof G);
+    int V,E;
+    cin>>V>>E;
+    memset(G,NULL,MAX_V);
+    for(int i=0;i<E;++i)
+        {
+        int desde,hasta,peso;
+        cin>>desde>>hasta;
+        cin>>peso;
+        G[desde][hasta]=peso;
+        G[hasta][desde]=peso;
+    }
+    for(int i=0;i<V;++i)
+    {
+        for(int j=0;j<V;++j)
+        {
+        cout<<G[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
