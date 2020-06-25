404c55b8885da29a485702ab009dfec576717e5c
Luciano890 <62488915+Luciano890@users.noreply.github.com>
2020-06-25 12:05:29 -0500

    Update DFS.cpp

--- C++/Luciano_Chavarria/Grafos/DFS.cpp
+++ C++/Luciano_Chavarria/Grafos/DFS.cpp
@@ -1,24 +1,6 @@
 #include <bits/stdc++.h>
 #define MAX_V 101
-//typedef pair Edge;
 using namespace std;
-/*
-class Graph{
-private:
-    int V;
-    vector<int> adj[];
-public:
-    Graph(int V,vector<int> []);
-    void addEdge(int v,int w);
-    void DFSUtil(int v,bool visited[]);
-    void DFS(int v);
-};
-Graph::Graph(int V_,vector<int>adj_[]){
-    std::vector<int>::iterator it;
-    V=V_;
-    adj[].insert(it+adj_.size(),adj_.begin(),adj_.end());
-}
-*/
 vector<int> adj[MAX_V];
 struct Graph{
     int V;
@@ -28,13 +10,12 @@ struct Graph{
     void DFSUtil(int v,bool visited[]){
         visited[v]=true;
         cout<<v<<" ";
-        vector<int>::iterator it=adj[v].begin();
-       while(it!=adj[v].end()){
-            if(!visited[*it]){
-                DFSUtil(*it,visited);
+        for(int i=0;i<adj[v].size();++i){
+            int n=adj[v][i];
+            if(!visited[n]){
+                DFSUtil(n,visited);
             }
-       *it++;
-       }
+        }
     }
      void DFS(int v){
         bool visited[V];
