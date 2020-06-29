92f542c33900f175f27766d274c408eed170ce09
Cristiank17 <62625689+Cristiank17@users.noreply.github.com>
2020-06-29 14:34:15 -0500

    Create Quick_Sort.java

---
+++ Java/Cristian Maldonado/Ordenamientos/Quick_Sort.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Quick_Sort {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String entrada[] = br.readLine().split(" ");
+        int arr[] = new int[entrada.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(entrada[i]);
+        }
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("Imprimiendo");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+    }
+
+    static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    static void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
+        }
+    }
+}
