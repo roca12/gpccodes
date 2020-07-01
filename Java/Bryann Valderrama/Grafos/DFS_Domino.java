4b3700ddf613ce135e78e36f14a639e113c6905c
BryanttV <64033729+BryanttV@users.noreply.github.com>
2020-07-01 08:43:55 -0500

    Create DFS_Domino.java

---
+++ Java/Bryann Valderrama/Grafos/DFS_Domino.java
@@ -0,0 +1,47 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    static final int MAX = 10001;
+    static ArrayList<ArrayList<Integer>> ady = new ArrayList<ArrayList<Integer>>();
+    static Scanner sc = new Scanner(System.in);
+    static int total;
+    static boolean visitado[] = new boolean[MAX];
+
+    static void DFS(int u) {
+        total++;
+        visitado[u] = true;
+        for (int v = 0; v < ady.get(u).size(); v++) {
+            if (!visitado[ady.get(u).get(v)]) {
+                DFS(ady.get(u).get(v));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int dominos, caminos, busquedas, desde, hasta, origen;
+        dominos = sc.nextInt();
+        caminos = sc.nextInt();
+        busquedas = sc.nextInt();
+        for (int i = 0; i < dominos; i++) {
+            ady.add(new ArrayList<>());
+        }
+        while (caminos > 0) {
+            desde = sc.nextInt();
+            hasta = sc.nextInt();
+            ady.get(desde).add(hasta);
+            caminos--;
+        }
+        while (busquedas > 0) {
+            origen = sc.nextInt();
+            total = 0;
+            Arrays.fill(visitado, false);
+            DFS(origen);
+            System.out.println("Total dominos tumbados: " + total);
+            busquedas--;
+        }
+    }
+}
