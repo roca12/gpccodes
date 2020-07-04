01f37b738bc6ba230cb4a152defc7757edb6ba8c
Cristiank17 <62625689+Cristiank17@users.noreply.github.com>
2020-07-04 14:04:04 -0500

    Create Tree_Sort.java

---
+++ Java/Cristian Maldonado/Ordenamientos/Tree_Sort.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TreeSort {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String entrada[] = br.readLine().split(" ");
+        int arr[] = new int[entrada.length];
+        for (int i = 0; i < entrada.length; i++) {
+            arr[i] = Integer.parseInt(entrada[i]);
+        }
+        treeInsert(arr);
+        inOrderRec(root);
+    }
+
+    static class Nodo {
+
+        int key;
+        Nodo izquierda, derecha;
+
+        public Nodo(int item) {
+            this.key = item;
+            izquierda = derecha = null;
+        }
+    }
+    static Nodo root;
+
+    static void init() {
+        root = null;
+    }
+
+    static void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    static Nodo insertRec(Nodo root, int key) {
+        if (root == null) {
+            root = new Nodo(key);
+            return root;
+        }
+        if (key < root.key) {
+            root.izquierda = insertRec(root.izquierda, key);
+        } else if (key >= root.key) {
+            root.derecha = insertRec(root.derecha, key);
+        }
+        return root;
+    }
+
+    static void inOrderRec(Nodo root) {
+        if (root != null) {
+            inOrderRec(root.izquierda);
+            System.out.print(root.key + " ");
+            inOrderRec(root.derecha);
+        }
+    }
+
+    static void treeInsert(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            insert(arr[i]);
+        }
+    }
+}
