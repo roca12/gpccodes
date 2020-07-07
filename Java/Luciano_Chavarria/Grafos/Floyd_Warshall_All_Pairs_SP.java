01c51a02798d2c02b230f34982ef21cfa4f940f3
Luciano890 <62488915+Luciano890@users.noreply.github.com>
2020-07-07 12:24:10 -0500

    Update Floyd_Warshall_All_Pairs_SP.java

--- Java/Luciano_Chavarria/Grafos/Floyd_Warshall_All_Pairs_SP.java
+++ Java/Luciano_Chavarria/Grafos/Floyd_Warshall_All_Pairs_SP.java
@@ -1,18 +1,23 @@
 // Programa java que busca todos los caminos más cortos
 // en un grafo
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     static int INF = 9999;
-     static int V=0;
+    static int V = 0;
+    //grafo en matriz fija
+    static int[][] graph;
+    //matriz que almacena el camino recorrido
+    static int[][] next;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] graph;
         V = sc.nextInt();
         graph = new int[V][V];
+        next = new int[V][V];
         int E = sc.nextInt();
         //Inicializa todo en infinito
         for (int i = 0; i < V; i++) {
@@ -20,6 +25,7 @@ public class Main {
                 graph[i][j] = INF;
             }
         }
+
         // De un nodo al mismo nodo es 0
         for (int i = 0; i < V; i++) {
             graph[i][i] = 0;
@@ -27,10 +33,14 @@ public class Main {
         for (int i = 0; i < E; i++) {
             graph[sc.nextInt()][sc.nextInt()] = sc.nextInt();
         }
-
         floydWarshall(graph);
+        ArrayList<Integer> path;
+        System.out.println("El camino mas corto desde 0 a 3: ");
+        //Construit camino
+        path = constructPath(0, 3);
+        //imprimir camino
+        printPath(path);
     }
-
 
     static void floydWarshall(int graph[][]) {
         int dist[][] = new int[V][V];
@@ -43,10 +53,16 @@ public class Main {
         for (i = 0; i < V; i++) {
             for (j = 0; j < V; j++) {
                 dist[i][j] = graph[i][j];
+                // Si no hay camino entre i y j
+                if (graph[i][j] == INF) {
+                    next[i][j] = -1;
+                } else {
+                    next[i][j] = j;
+                }
             }
         }
 
-        /*Agregue todos los vértices uno por uno al conjunto 
+            /*Agregue todos los vértices uno por uno al conjunto 
             de vértices intermedios.
             ---> Antes del inicio de una iteración, tenemos 
             distancias más cortas entre todos los pares de vértices, 
@@ -64,14 +80,42 @@ public class Main {
                 for (j = 0; j < V; j++) {
                     // Si vertice k esa en el camino más corto
                     // desde i a j, actualiza el valor de dist[i][j]
-                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
-                        dist[i][j] = dist[i][k] + dist[k][j];
+                    // No podemos viajar por un camino si no existe
+                    if (dist[i][k] == INF|| dist[k][j] == INF) {
+                        continue;
+                    }
+                    if (dist[i][j] > dist[i][k]+ dist[k][j]) {
+                        dist[i][j] = dist[i][k]+ dist[k][j];
+                        next[i][j] = next[i][k];
                     }
                 }
             }
         }
         //Imprime la matriz de distancias cortas
         printSolution(dist);
+    }
+
+    static ArrayList<Integer> constructPath(int u, int v) {
+        // si no hay camino devuelve una lista vacia
+        if (next[u][v] == -1) {
+            return new ArrayList<>();
+        }
+        // Almacenando el camino en el vector
+        ArrayList<Integer> path = new ArrayList<>();
+        path.add(u);
+        while (u != v) {
+            u = next[u][v];
+            path.add(u);
+        }
+        return path;
+    }
+
+    static void printPath(ArrayList<Integer> path) {
+        int n = path.size();
+        for (int i = 0; i < n - 1; i++) {
+            System.out.print(path.get(i) + " -> ");
+        }
+        System.out.println(path.get(n - 1) + " -> ");
     }
 
     static void printSolution(int dist[][]) {
@@ -89,3 +133,4 @@ public class Main {
         }
     }
 }
+
