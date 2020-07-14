4ba357cbaa926c8b61a492fe50b26ebd2c7722ba
Luciano890 <62488915+Luciano890@users.noreply.github.com>
2020-07-14 11:28:58 -0500

    Create Knuth_Morris_Pratt_Matching.py

---
+++ PY/Luciano_Chavarria/Strings/Knuth_Morris_Pratt_Matching.py
@@ -0,0 +1,47 @@
+def KMPsearch(pat,txt):
+	M = len(pat)
+	N = len(txt)
+
+	lps = [None for x in range(M)]
+	j = 0
+
+	computeLPSArray(pat, M, lps)
+
+	i = 0
+
+	while i < N:
+		if pat[j] == txt[i]:
+			i+=1
+			j+=1
+		if j == M:
+			print("Encontrado patrón (" , pat , ") en el indice " , (i - j) , "-" , ((i - j) + M - 1))
+			j = lps[j - 1]
+		elif i < N and pat[j] != txt[i]:
+			if j != 0:
+				j = lps[j-1]
+			else:
+				i+=1
+
+def computeLPSArray(pat,M,lps):
+	leng = 0
+	i = 1
+	while i < M:
+		if pat[i] == pat[leng]:
+			leng+=1
+			lps[i] = leng
+			i+=1
+		else:
+			if leng != 0:
+				leng = lps[leng-1]
+			else:
+				lps[i] = leng
+				i+=1
+
+def variasBusquedas(arr,txt):
+	for i in range(len(arr)):
+		KMPsearch(arr[i],txt)
+
+if __name__ == '__main__':
+	txt = "lalalalalalalalalalala"
+	arr = ["la", "lal", "lala"]
+	variasBusquedas(arr, txt)
