c16cdc6d6f590ec193fb71e8fb0758ad7d431242
Cristiank17 <62625689+Cristiank17@users.noreply.github.com>
2020-07-20 19:03:22 -0500

    Create Cycle_In_A_Directed_Graph.java

---
+++ Java/Cristian Maldonado/Grafos/Cycle_In_A_Directed_Graph.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Cycle_In_A_Directed_Graph {
+
+  
+    public static void main(String[] args) {
+        Graph graph = new Graph(4);
+            graph.addEdge(0, 1);
+            graph.addEdge(0, 2);
+            graph.addEdge(1, 2);
+            graph.addEdge(2, 0);
+            graph.addEdge(2, 3);
+            graph.addEdge(3, 3);
+            if (graph.isCyclic()) {
+                System.out.println("El grafo contiene un ciclo");
+            } else {
+                System.out.println("El grafo no contiene"
+                        + " un ciclo");
+            }
+    }
+    
+    static class Graph{
+        
+        int V;
+        List<List<Integer>> adj;
+
+        public Graph(int v) {
+            this.V = v;
+            adj = new ArrayList<>(v);
+            for (int i = 0; i < v; i++) {
+                adj.add(new LinkedList<>());
+            }
+        }
+        void addEdge(int source, int dest){
+            adj.get(source).add(dest);
+        }
+        boolean isCycliUtil(int i, boolean [] visited, boolean [] recStack){
+            if(recStack[i]){
+                return true;
+            }
+            if(visited[i]){
+                return false;
+            }
+            visited[i] = true;
+            recStack[i] = true;
+            List<Integer> children =adj.get(i);
+            if(children.stream().anyMatch((c) ->isCycliUtil(c, visited, recStack))){
+                return true;
+            }
+            recStack[i] = false;
+            return false;
+        }
+        
+        boolean isCyclic(){
+            boolean[] visited = new boolean[V];
+            boolean [] recStack = new boolean[V];
+            for (int i = 0; i < V; i++) {
+                if(isCycliUtil(i, visited, recStack)){
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+    }
+}
