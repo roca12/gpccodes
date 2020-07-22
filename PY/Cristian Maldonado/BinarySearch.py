a6bf44f27d03aa48cf43d7f2a6eff71f911fd9cd
BryanttV <64033729+BryanttV@users.noreply.github.com>
2020-07-21 20:42:00 -0500

    Update BinarySearch.py

--- PY/Cristian Maldonado/BinarySearch.py
+++ PY/Cristian Maldonado/BinarySearch.py
@@ -1,19 +1,19 @@
-def BinarySearch(arr, inicio, fin, numerobuscado):
-    if(fin>=inicio):
-        medio = int(inicio+(fin-1)/2)
-        if(arr[medio] == numerobuscado):
+def binarySearch(arr, izq, der, numeroabuscar):
+    if(der >= izq):
+        medio = izq + (der-izq) // 2
+        if(arr[medio] == numeroabuscar):
             return medio
-        if(arr[medio] > numerobuscado):
-            return BinarySearch(arr,inicio,medio-1,numerobuscado)
+        if (arr[medio] > numeroabuscar):
+            return binarySearch(arr, izq, medio-1, numeroabuscar)
         else:
-            return BinarySearch(arr,medio+1,fin,numerobuscado)
+            return binarySearch(arr, medio+1, der, numeroabuscar)
     return -1
 
-arr = [1,2,3,4,5,6]
-tam = len(arr)
-numerobuscado = 7
-resultado = int (BinarySearch(arr,0,tam-1,numerobuscado))
+
+arr = [2, 3, 4, 5, 6]
+numeroabuscar = int(input())
+resultado = binarySearch(arr, 0, len(arr)-1, numeroabuscar)
 if(resultado == -1):
-    print("Elemento no encontrado")
+    print('Elemento no encontrado')
 else:
-    print("Elemento encontrado en la posicion", resultado)
+    print('Elemento encontrado en el indice', resultado)
