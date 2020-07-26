ee07ecd5e9b4293f2e0698d9d535f686c0d511c2
Cristiank17 <62625689+Cristiank17@users.noreply.github.com>
2020-07-26 13:27:17 -0500

    Create Kruskal_Minimum_Spanning_Tree.java

---
+++ Java/Cristian Maldonado/Grafos/Kruskal_Minimum_Spanning_Tree.java
@@ -0,0 +1,88 @@
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Kruskal_Minimum_Spanning_Tree {
+
+    static final int MAX = 1005;
+    static int padre[] = new int[MAX];
+
+    static void makeSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            padre[i] = i;
+        }
+    }
+
+    static int find(int x) {
+        return (x == padre[x] ? x : (padre[x] = find(padre[x])));
+    }
+
+    static void union(int x, int y) {
+        padre[find(x)] = find(y);
+    }
+
+    static boolean sameComponent(int x, int y) {
+        if (find(x) == find(y)) {
+            return true;
+        }
+        return false;
+    }
+
+    static int V, E;
+
+    static class Edge implements Comparator<Edge> {
+
+        int origen, destino, peso;
+
+        @Override
+        public int compare(Edge t, Edge t1) {
+            return t.peso - t1.peso;
+        }
+    }
+
+    static Edge[] aristas = new Edge[MAX];
+    static Edge MST[] = new Edge[MAX];
+
+    static void kruskalMST() {
+        int origen, destino, peso;
+        int total = 0;
+        int numAristas = 0;
+        makeSet(V);
+        Arrays.sort(aristas, 0, E, new Edge());
+        for (int i = 0; i < E; i++) {
+            origen = aristas[i].origen;
+            destino = aristas[i].destino;
+            peso = aristas[i].peso;
+            if (!sameComponent(origen, destino)) {
+                total += peso;
+                MST[numAristas++] = aristas[i];
+                union(origen, destino);
+            }
+        }
+        if (V - 1 != numAristas) {
+            System.out.println("No existe MST valido para el grafo ingresado, este debe ser conexo");
+            return;
+        }
+        System.out.println("El MST enconstrado contiene las siguientes aristas: ");
+        for (int i = 0; i < numAristas; i++) {
+            System.out.printf("(%d, %d): %d\n", MST[i].origen, MST[i].destino, MST[i].peso);
+        }
+        System.out.println("El costo minimo de todas las aristas del MST es: " + total);
+    }
+
+ //Manejo de grafos no dirigidos   
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        V = sc.nextInt();
+        E = sc.nextInt();
+        for (int i = 0; i < E; i++) {
+            aristas[i] = new Edge();
+            aristas[i].origen = sc.nextInt();
+            aristas[i].destino = sc.nextInt();
+            aristas[i].peso = sc.nextInt();
+        }
+        kruskalMST();
+    }
+}
