44e17a8d1d31ffd702ad13038e7496e1f19d16c7
Luciano890 <62488915+Luciano890@users.noreply.github.com>
2020-07-28 11:46:21 -0500

    Create Combinatoria.py

---
+++ PY/Luciano_Chavarria/Matemáticas/Combinatoria.py
@@ -0,0 +1,26 @@
+from sys import stdout
+wr = stdout.write
+
+
+def combUtil(arr, data, start, end, index, r):
+    if index == r:
+        for j in range(r):
+            wr(f'{data[j]} ')
+        wr('\n')
+        return
+    i = start
+    while i <= end and end - i + 1 >= r - index:
+        data[index] = arr[i]
+        combUtil(arr, data, i+1, end, index+1, r)
+        i += 1
+
+
+def printComb(arr, n, r):
+    data = [0 for x in range(r)]
+    combUtil(arr, data, 0, n-1, 0, r)
+
+
+arrint = [1, 2, 3, 4, 5]
+r = 3
+n = len(arrint)
+printComb(arrint, n, r)
