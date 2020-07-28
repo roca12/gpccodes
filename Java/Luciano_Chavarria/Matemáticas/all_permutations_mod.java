1082221205dfbbf37fd5113749ba20e5f97c7df9
Luciano890 <62488915+Luciano890@users.noreply.github.com>
2020-07-28 11:33:52 -0500

    Create all_permutations_mod.java

---
+++ Java/Luciano_Chavarria/Matemáticas/all_permutations_mod.java
@@ -0,0 +1,57 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Main {
+
+    static Set<String> permutaciones;
+    static Set<String> result = new HashSet<>();
+
+    static void shuffle(char c) {
+        if (permutaciones.isEmpty()) {
+            permutaciones.add(String.valueOf(c));
+        } else {
+            Iterator<String> it = permutaciones.iterator();
+            for (int i = 0; i < permutaciones.size(); i++) {
+                String temp1;
+                while (it.hasNext()) {
+                    temp1 = it.next();
+                    for (int k = 0; k < temp1.length() + 1; k++) {
+                        StringBuilder sb = new StringBuilder(temp1);
+                        sb.insert(k, c);
+                        result.add(sb.toString());
+                    }
+                }
+            }
+            permutaciones = result;
+            result = new HashSet<>();
+        }
+    }
+
+    static Set<String> permutation(String string) {
+        permutaciones = new HashSet<>();
+        int n = string.length();
+        for (int i = n - 1; i >= 0; i--) {
+            shuffle(string.charAt(i));
+        }
+        Set<String> aux = new  HashSet<>();
+        Iterator<String> it = permutaciones.iterator();
+        while (it.hasNext()) {
+            String aux2=it.next();
+            //Entre mas aumente r, mas pequeñas seran las permutaciones
+            int r=0;
+            aux.add(aux2.substring(0, aux2.length()-0));
+        }
+        return aux;
+
+    }
+
+    public static void main(String[] args) {
+        Set<String> res = permutation("1234");
+        System.out.println("Hay en total " + res.size() + " permutaciones");
+        Iterator<String> it = res.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
