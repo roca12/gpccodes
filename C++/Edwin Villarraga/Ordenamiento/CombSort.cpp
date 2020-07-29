1a965c7589f6d620e33ddf21a1514b946a2e4c30
edanv1401 <62625858+edanv1401@users.noreply.github.com>
2020-07-29 12:13:50 -0500

    Create CombSort.cpp

---
+++ C++/Edwin Villarraga/Ordenamiento/CombSort.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#include<cstdlib>
+using namespace std;
+int getNextGap(int gap){
+    gap=(gap*10)/13;
+    if(gap<1){
+        return 1;
+    }
+    return gap;
+}
+void sortArr(int arr[],int n){
+    int gap=n;
+    bool swapped=true;
+    while(gap!=1 || swapped == true){
+        gap=getNextGap(gap);
+        swapped=false;
+        for(int i=0;i<n-gap;i++){
+            if(arr[i]>arr[i+gap]){
+                swap(arr[i],arr[i+gap]);
+            swapped=true;
+            }
+        }
+    }
+}
+void printArr(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main() {
+    int arr[]={8,4,1,56,3,-44,23,-6,28,0};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    sortArr(arr,n);
+    cout<<"arreglo ordenado"<<endl;
+    printArr(arr,n);
+}
