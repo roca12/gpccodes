b84e38eb113911c8a38e5cab621c52c3524197ce
edanv1401 <62625858+edanv1401@users.noreply.github.com>
2020-07-29 10:09:30 -0500

    Create 11044_uva.cpp

---
+++ Resueltos/Edwin Villarraga/11044_uva.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<cstdlib>
+using namespace std;
+int main() {
+    int c;
+    cin>>c;
+    for(int i=0;i<c;i++){
+        int a,b;
+        cin>>a>>b;
+        cout<<(a/3)*(b/3)<<endl;
+    }
+}
