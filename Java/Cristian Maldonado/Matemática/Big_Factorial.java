af05dd71af413cb67f7b11091a7c7bce506cce2a
Cristiank17 <62625689+Cristiank17@users.noreply.github.com>
2020-07-30 20:29:09 -0500

    Create Big_Factorial.java

---
+++ Java/Cristian Maldonado/Matemática/Big_Factorial.java
@@ -0,0 +1,35 @@
+public class Big_Factorial {
+    
+    static int multiply(int x, int res[], int res_size){
+        int carry = 0;
+        for (int i = 0; i < res_size; i++) {
+            int prod = res[i] * x + carry;
+            res[i] = prod % 10;
+            carry = prod / 10;
+        }
+        while(carry !=0){
+            res[res_size] = carry % 10;
+            carry /= 10;
+            res_size++;
+        }
+        return res_size;
+    }
+
+    static void factorial(int n) {
+        int res[] = new int[10000];
+        res[0] = 1;
+        int res_size = 1;
+        for (int i = 2; i <= n; i++) {
+            res_size = multiply(i, res, res_size);
+        }
+        System.out.println("Factorial del numero dado es:");
+        for (int i = res_size - 1; i >= 0; i--) {
+            System.out.print(res[i]);
+        }
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        factorial(100);
+    }
+}
