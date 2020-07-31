cadbfcf8fb03b63fc2dc99364f23b0510270884e
BryanttV <64033729+BryanttV@users.noreply.github.com>
2020-07-30 21:18:35 -0500

    Create a%x=b.py

---
+++ PY/Bryann Valderrama/Matemática/a%x=b.py
@@ -0,0 +1,30 @@
+import math
+from sys import stdout
+wr = stdout.write
+
+
+def modEquation(a, b):
+    if a < b:
+        wr(f'No hay Solucion')
+        return
+    if a == b:
+        wr(f'Infinitas Soluciones')
+    cont = 0
+    n = a - b
+    y = int(math.sqrt(a-b))
+    for i in range(1, y+1):
+        if n % i == 0:
+            if n // i > b:
+                cont += 1
+                wr(f'{n//i}\n')
+            if i > b:
+                con += 1
+                wr(f'{i}\n')
+    if y*y == n and y > b:
+        cont -= 1
+    wr(f'{cont}')
+
+
+a = 21
+b = 5
+modEquation(a, b)
