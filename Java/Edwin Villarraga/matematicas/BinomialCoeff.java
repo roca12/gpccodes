7652197a60ac06ac887071874e015a079752ae91
edanv1401 <62625858+edanv1401@users.noreply.github.com>
2020-08-03 09:38:29 -0500

    Create BinomialCoeff.java

---
+++ Java/Edwin Villarraga/matematicas/BinomialCoeff.java
@@ -0,0 +1,13 @@
+public class Main {
+	static int binomialCoeff(int n, int k) {
+        if (k == 0 || k == n) {
+            return 1;
+        }
+        return binomialCoeff(n - 1, k - 1)+ binomialCoeff(n - 1, k);
+    }
+    public static void main(String args[]) {
+        int n = 5, k = 2;
+        System.out.printf("Valor de C(%d, %d) is %d ", n, k, binomialCoeff(n, k));
+
+    }
+}
