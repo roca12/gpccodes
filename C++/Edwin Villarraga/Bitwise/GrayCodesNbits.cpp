375d9dba6cd63198202f0a3bfc06953aadc0487a
edanv1401 <62625858+edanv1401@users.noreply.github.com>
2020-08-12 14:47:19 -0500

    Create GrayCodesNbits.cpp

---
+++ C++/Edwin Villarraga/Bitwise/GrayCodesNbits.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include<cstdlib>
+//---------------//
+using namespace std;
+void generateGray(int n){
+    if(n<=0){
+        return;
+    }
+    vector<string>arr;
+    arr.push_back("0");
+    arr.push_back("1");
+    for(int i=2;i<(1<<n);i <<=1){
+        for(int j = i-1;j>=0;j--){
+            arr.push_back(arr[j]);
+        }
+        for(int j=0;j<i;j++){
+            arr[j]="0"+arr[j];
+        }
+        for(int j=i;j<2*i;j++){
+            arr[j]="1"+arr[j];
+        }
+    }
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<endl;
+    }
+}
+int main(){
+
+    generateGray(5);
+}
