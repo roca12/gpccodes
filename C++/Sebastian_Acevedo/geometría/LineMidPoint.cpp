f1e102f68883da5d4f17c580df57896a2cf57c0a
5AresAce6 <70976289+5AresAce6@users.noreply.github.com>
2020-09-09 21:46:24 -0500

    Update LineMidPoint.cpp

--- C++/Sebastian_Acevedo/geometría/LineMidPoint.cpp
+++ C++/Sebastian_Acevedo/geometría/LineMidPoint.cpp
@@ -1,13 +1,22 @@
-#include<bits/stdc++.h>
-#include<cstdlib>
+#include <iostream>
+#include <String>
+//linemidepoint
 using namespace std;
-string midPoint(int x1,int y1,int x2,int y2){
-    return (to_string((x1+x2)/2)+","+to_string((y1+y2)/2));
-}
-int main() {
-    int x1,y1;
-    int x2,y2;
-    cin>>x1>>y1>>x2>>y2;
-    string medio=midPoint(x1,y1,x2,y2);
-    cout<<medio<<endl;
+string midpoint(int x1, int y1, int x2, int y2) {
+	int x, y;
+	x=(x1+x2)/2;
+	y=(y1+y2)/2;
+	string res = (x + ","+ y);
+	return res;
+	}
+int main(int argc, char** argv) {
+	int x1,x2,y1,y2;
+	//primer punto
+	cin>>x1;
+	cin>>y1;
+	//segundo punto
+	cin>>x2;
+	cin>>y2;
+	string medio = midpoint(x1,y1,x2,y2);
+	cout<<medio<<endl;
 }
