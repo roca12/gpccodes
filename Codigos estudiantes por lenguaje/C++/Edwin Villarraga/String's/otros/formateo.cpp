a1921700f59797d228a8a1e9976907eb908eb5c7
edanv1401 <62625858+edanv1401@users.noreply.github.com>
2020-09-16 14:56:12 -0500

    Create formateo.cpp

---
+++ Codigos estudiantes por lenguaje/C++/Edwin Villarraga/String's/otros/formateo.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+#include <cstdlib>
+using namespace std;
+const string formato ="Case #%d\n%.2f %.1f\n";
+int main() {
+    int cont=1;
+    printf(formato.c_str(),cont,3.1416,2.3);
+    return 0;
+}
