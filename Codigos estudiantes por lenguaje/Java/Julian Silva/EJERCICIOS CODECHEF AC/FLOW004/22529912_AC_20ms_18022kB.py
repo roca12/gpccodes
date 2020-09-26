c65c46ac02a65404de7a3d4448e6f95beaa3b460
Diego Rodriguez <44822667+roca12@users.noreply.github.com>
2020-09-26 10:49:51 -0500

    no message

--- Codigos estudiantes por lenguaje/Java/Julian Silva/EJERCICIOS CODECHEF AC/FLOW004/22529912_AC_20ms_18022kB.py
+++
@@ -1,6 +0,0 @@
-for i in range(int(input())):
-    x=input()
-    y=int(x[0])
-    z=int(x[len(x)-1])
-    sum=y+z
-    print(sum)
--- Codigos estudiantes por lenguaje/Java/Julian Silva/EJERCICIOS CODECHEF AC/MUFFINS3/22530003_AC_20ms_18022kB.py
+++
@@ -1,7 +0,0 @@
-for i in range (int(input())):
-    n = int(input())
-    if (n % 2 == 0):
-        a=((n/2)+1)
-    else:
-        a=((n+1)/2)
-    print(int(a))    
