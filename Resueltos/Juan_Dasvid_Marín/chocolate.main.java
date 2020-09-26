af79f0600438b4d0293a56d57d652d2780d9f471
Juan David Marín <wvany1996@gmail.com>
2020-09-26 16:27:40 -0500

    respondidos

---
+++ Resueltos/Juan_Dasvid_Marín/chocolate.main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String... args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println((a*b)-1);
+        }
+    }
+}
