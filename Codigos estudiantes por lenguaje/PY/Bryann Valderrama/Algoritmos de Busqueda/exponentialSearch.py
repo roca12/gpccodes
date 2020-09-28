4ec6da12aa85e95af57f9394b3e7a4aed642a0d7
BryanttV <64033729+BryanttV@users.noreply.github.com>
2020-09-28 17:35:34 -0500

    Update exponentialSearch.py

--- Codigos estudiantes por lenguaje/PY/Bryann Valderrama/Algoritmos de Busqueda/exponentialSearch.py
+++ Codigos estudiantes por lenguaje/PY/Bryann Valderrama/Algoritmos de Busqueda/exponentialSearch.py
@@ -1,30 +1,47 @@
-def binarySearch(arr, inicio, fin, numeroabuscar):
-    if(fin > inicio):
-        medio = int((inicio+(fin-1))/2)
-        if(arr[medio] == numeroabuscar):
-            return medio
-        if(arr[medio] > numeroabuscar):
-            return binarySearch(arr, inicio, medio-1, numeroabuscar)
+'''Exponencial Search
+
+   - La búsqueda crece exponencialmente.
+   - Útil para vectores dinámicos.
+   - Complejidad Tiempo: O(log n).
+'''
+
+from sys import stdin, stdout
+rl = stdin.readline
+wr = stdout.write
+
+
+def binary_Search(arr, left, right, x):
+    if right >= left:
+
+        mid = left + (right - left) // 2
+
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return binary_Search(arr, left, mid-1, x)
         else:
-            return binarySearch(arr, medio+1, fin, numeroabuscar)
-    return -1
+            return binary_Search(arr, mid+1, right, x)
 
-
-def exponentialSearch(arr, tam, numeroabuscar):
-    if(arr[0] == numeroabuscar):
-        return 0
-    i = int(1)
-    while (i < tam and arr[i] <= numeroabuscar):
-        i = i*2
-    return binarySearch(arr, i//2, min(i, tam), numeroabuscar)
-
-
-if __name__ == '__main__':
-    lista = [1, 2, 3, 4, 5, 6]
-    tamanio = len(lista)
-    numeroabuscar = int(6)
-    resultado = int(exponentialSearch(lista, tamanio, numeroabuscar))
-    if(resultado < 0):
-        print("Elemento no presente")
     else:
-        print("Elemento encuentrado en el indice:", resultado)
+        return -1
+
+
+def exponential_Search(arr, n, x):
+    if arr[0] == x:
+        return 0
+
+    i = 1
+    while i < n and arr[i] <= x:
+        i = i * 2
+
+    return binary_Search(arr, i//2, min(i, n), x)
+
+
+arr = list(map(int, rl().split()))
+x = int(rl())
+result = exponential_Search(arr, len(arr)-1, x)
+
+if result == -1:
+    wr('Dato no encontrado')
+else:
+    wr(f'Dato encontrado en la posicion {result}')
