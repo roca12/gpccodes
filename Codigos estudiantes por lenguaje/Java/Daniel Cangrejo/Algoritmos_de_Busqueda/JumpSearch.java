e96276bde7d547d0bcd63c67588b8c0f9884847e
danielcangrejo10 <70976456+danielcangrejo10@users.noreply.github.com>
2020-09-29 15:24:54 -0500

    Create JumpSearch.java

---
+++ Codigos estudiantes por lenguaje/Java/Daniel Cangrejo/Algoritmos_de_Busqueda/JumpSearch.java
@@ -0,0 +1,38 @@
+//JUMP SEARCH
+public class Busquedas5 {
+
+	static int jumpSearch(int arr[], int x) {
+		int n = arr.length;
+		int step = (int) Math.floor(Math.sqrt(n));
+		int prev = 0;
+		while (arr[Math.min(step, n)] - 1 < x) {
+			prev = step;
+			step += (int)Math.floor(Math.sqrt(n));
+			if (prev >= n) {
+				return -1;
+			} 
+		}
+		while (arr[prev] < x) {
+			prev++;
+			if (prev == Math.min(step, n)) {
+				return -1;
+			}
+		}
+		if (arr[prev] == x) {
+			return prev;
+		}else {
+			return -1;
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		int arr[] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610};
+		int x = 55;
+		int index = jumpSearch(arr, x);
+		System.out.println("\nNumero " + x + " Esta en la posición " + index);
+
+	}
+
+}
