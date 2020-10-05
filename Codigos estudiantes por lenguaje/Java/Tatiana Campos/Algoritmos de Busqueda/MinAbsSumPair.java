d60a1bd6034afa7a8c318887bec515a1566cccb6
tatiana00017 <71901494+tatiana00017@users.noreply.github.com>
2020-10-05 13:50:01 -0500

    Create MinAbsSumPair.java

---
+++ Codigos estudiantes por lenguaje/Java/Tatiana Campos/Algoritmos de Busqueda/MinAbsSumPair.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    static void minAbsSumPair(int arr[], int arrsize) {
+        int left, right, min_sum, sum, minl, minr;
+        if (arrsize < 2) {
+            System.out.println("invalido");
+            return;
+        }
+        minl = 0;
+        minr = 1;
+        min_sum = arr[0] + arr[1];
+        for (left = 0; left < arrsize - 1; left++) {
+            for (right = left + 1; right < arrsize; right++) {
+                sum = arr[left] + arr[right];
+                if (Math.abs(min_sum) > Math.abs(sum)) {
+                    min_sum = sum;
+                    minl = left;
+                    minr = right;
+                }
+            }
+        }
+        System.out.println(" Los dos elementos los cuales "
+                + "tienen la suma minima son "
+                + arr[minl] + " y " + arr[minr] + "= " + min_sum);
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 60, -10, 70, -80, 85};
+        minAbsSumPair(arr, arr.length);
+
+    }
+}
