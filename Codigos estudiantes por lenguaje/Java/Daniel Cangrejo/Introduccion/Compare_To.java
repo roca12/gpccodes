42c49bb4a4a7e9aaa6d83a0080df853244071893
danielcangrejo10 <70976456+danielcangrejo10@users.noreply.github.com>
2020-10-08 15:39:01 -0500

    Create Compare_To.java

---
+++ Codigos estudiantes por lenguaje/Java/Daniel Cangrejo/Introduccion/Compare_To.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Comparate_To {
+
+	static class Persona implements Comparable<Persona> {
+        String nombre;
+        Integer edad;
+        public Persona(String nombre, Integer edad) {
+            this.nombre=nombre;
+            this.edad= edad;
+        }
+        @Override
+        public int compareTo(Persona otro) {
+            if (this.edad>otro.edad) {
+                return 1;
+            }else if (this.edad==otro.edad) {
+                return 0;
+            }
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Persona roca= new Persona("Diego",23);
+        Persona el_mas_sano = new Persona("Stiven", 19);
+        Persona kpo = new Persona("Bryann", 20);
+        Persona daniel = new Persona("Dtiven", 19);
+        Persona juas_juas = new Persona("Daniel", 17);
+        Persona impostor = new Persona ("Yeison", 17);
+        Persona juano = new Persona ("Juan",19);
+        Persona el_de_nuestros_corazones= new Persona("Edwin", 18);
+        Persona el_nuevo = new Persona("Johan", 16);
+        Persona ares = new Persona("Sebastian", 19);
+        Persona fuerzaedwin = new Persona("El luchador", 19);
+        Persona dianita= new Persona("Diana", 18);
+        
+        ArrayList<Persona> lossaltamontes = new ArrayList<>();
+        lossaltamontes.add(roca);
+        lossaltamontes.add(el_mas_sano);
+        lossaltamontes.add(juano);
+        lossaltamontes.add(kpo);
+        lossaltamontes.add(ares);
+        lossaltamontes.add(fuerzaedwin);
+        lossaltamontes.add(juas_juas);
+        lossaltamontes.add(daniel);
+        lossaltamontes.add(impostor);
+        lossaltamontes.add(el_nuevo);
+        lossaltamontes.add(dianita);
+        lossaltamontes.add(el_de_nuestros_corazones);
+        Collections.sort(lossaltamontes);
+        for (Persona p : lossaltamontes) {
+            System.out.println(p.nombre+"->"+p.edad);
+        }
+        
+    }
+}
