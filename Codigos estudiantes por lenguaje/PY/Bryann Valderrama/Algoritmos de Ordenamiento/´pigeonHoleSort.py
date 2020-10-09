ffd9bd08b1672ffd456bae62d50e96a9a1be6d66
BryanttV <64033729+BryanttV@users.noreply.github.com>
2020-10-09 15:13:05 -0500

    Create ´pigeonHoleSort.py

---
+++ Codigos estudiantes por lenguaje/PY/Bryann Valderrama/Algoritmos de Ordenamiento/´pigeonHoleSort.py
@@ -0,0 +1,43 @@
+'''PigeonHole Sort
+ 
+   - Adecuado para ordenar listas donde el num de elementos 
+     y el rango de valores de clave posible es aprox. igual.
+   - Si algun valor de la lista es muy grande o pequeño, no funciona.
+   - Similar a CountingSort
+   - Complejidad Tiempo:
+        - Caso Promedio: O(n+rango).
+'''
+
+from sys import stdin, stdout
+rl = stdin.readline
+wr = stdout.write
+
+
+def pigeonHoleSort(arr, n):
+
+    min_ = arr[0]
+    max_ = arr[0]
+
+    for a in range(n):
+        if arr[a] > max_:
+            max_ = arr[a]
+        if arr[a] < min_:
+            min_ = arr[a]
+
+    rango = max_ - min_ + 1
+    phole = [0] * rango
+
+    for i in range(n):
+        phole[arr[i] - min_] += 1
+
+    index = 0
+    for j in range(rango):
+        while phole[j] > 0:
+            phole[j] -= 1
+            arr[index] = j + min_
+            index += 1
+
+
+arr = list(map(int, rl().split()))
+pigeonHoleSort(arr, len(arr))
+print(*arr)
