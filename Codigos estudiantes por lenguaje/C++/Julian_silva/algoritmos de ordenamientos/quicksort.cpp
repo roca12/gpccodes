e0826c6461d6aa582f927875e3c088e0cf0d321c
juliansilvit <62625597+juliansilvit@users.noreply.github.com>
2020-10-12 12:05:36 -0500

    Create quicksort.cpp

---
+++ Codigos estudiantes por lenguaje/C++/Julian_silva/algoritmos de ordenamientos/quicksort.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <bits/stdc++.h>
+#define printArr(x,n) for(int i=0;i<n;i++){cout<<x[i]<<" ";};
+using namespace std;
+int partitionQ(int arr[],int low,int high){
+    int pivot=arr[high];
+    int i=(low-1);//index
+    for(int j=low;j<high;j++){
+        if(arr[j]<=pivot){
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+    swap(arr[i+1],arr[high]);
+    return i+1;
+}
+void quickSort(int arr[],int low,int high){
+    if(low<high){
+        int pi=partitionQ(arr,low,high);
+        quickSort(arr,low,pi-1);//sub vector izquierda
+        quickSort(arr,pi+1,high);//sub vector derecha
+    }
+}
+int main(){
+    int arr[]={2,3,4,5,6,2,23,4,6,2};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    printArr(arr,n);
+    cout<<endl;
+    quickSort(arr,0,n-1);
+    printArr(arr,n);
+}
