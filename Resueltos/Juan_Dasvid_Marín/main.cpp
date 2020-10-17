efbb5cff18f9b431da5532b2b84aa58eb5cf81f5
Juan David Marín <wvany1996@gmail.com>
2020-10-17 18:23:05 -0500

    ejercicios resueltos

---
+++ Resueltos/Juan_Dasvid_Marín/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int I,A,B;
+    for(
+        cin>>I;I--;
+        cout<<(B?(B>0?2-(B&1):1+(-B&1)):0)<<endl
+        )
+        cin>>A>>B, B-=A;
+}
+
+// Iteración ternaria dentro de for 
