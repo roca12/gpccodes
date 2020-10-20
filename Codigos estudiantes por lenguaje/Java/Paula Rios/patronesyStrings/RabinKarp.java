b3b20e179fb205f35896bf612afe7594ce518c45
PaulaRios14 <71901320+PaulaRios14@users.noreply.github.com>
2020-10-19 22:09:43 -0500

    Create RabinKarp.java

---
+++ Codigos estudiantes por lenguaje/Java/Paula Rios/patronesyStrings/RabinKarp.java
@@ -0,0 +1,42 @@
+public class Main {
+       final static int d=256;
+       static void RKsearch(String pat,String txt, int q){
+           int M=pat.length();
+           int N=txt.length();
+           int i,j;
+           int p=0;
+           int t=0;
+           int h=1;
+           for ( i = 0; i <M; i++) {
+               h=(h*d)%q;//almacena hash del patron
+           }
+           for ( i = 0; i <M; i++) {
+               p=(d*p+pat.charAt(i)%q);
+               t=(d*t+txt.charAt(i)%q);
+           }
+           for ( i = 0; i <=N-M; i++) {
+               if(p==t){
+                   for ( j = 0; j < M; j++) {
+                       if(txt.charAt(j)!=pat.charAt(j)){
+                           break;
+                       }
+                   }
+                   if(j==M){
+                       System.out.println("patron encontrado en el indice "+i+"-"+(i+M-1));
+                   }
+               }
+               if(i<N-M){
+                   t=(d*(t-txt.charAt(i)*h)+txt.charAt(i+M)%q);
+                   if(t<0){
+                       t=t+q;
+                   }
+               }
+           }
+       }
+    public static void main(String[] args) {
+        String txt="ANITALAVALATINA";
+        String pat="ANITA";
+        RKsearch(pat, txt, 7);     
+    }
+    
+}
