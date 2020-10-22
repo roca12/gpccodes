175df958d889d8314bc51dc7a26fba817776967f
Yeison Niño <44381397+Morktem@users.noreply.github.com>
2020-10-21 21:41:50 -0500

    Create f.java

---
+++ Resueltos/Yeison Niño/211020 Toy malito/f.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class Main {
+    
+    
+    public static void main(String[] args) {
+        Scanner tc = new Scanner(System.in);
+        int n = tc.nextInt();
+        int p = tc.nextInt();
+        for (int i = 0; i <= n; i++) {
+            String h = tc.nextLine();
+        }
+        System.out.println(p);
+    }
+}
