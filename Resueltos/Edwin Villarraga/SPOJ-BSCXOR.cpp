bdd2c0da14206c955595b2691e625d8e9d91b8a3
edanv1401 <62625858+edanv1401@users.noreply.github.com>
2020-10-22 15:53:22 -0500

    Create SPOJ-BSCXOR.cpp

---
+++ Resueltos/Edwin Villarraga/SPOJ-BSCXOR.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<cstdlib>
+#define MAX_SIZE 1
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);
+//---------------//
+using namespace std;
+typedef long long int ll;
+int main() {
+    int a,b;
+    cin>>a>>b;
+    cout<<(a^b)<<endl;
+}
