53b82122cb395dce9fb9630015a19caa39381fb2
Yeison Niño <44381397+Morktem@users.noreply.github.com>
2020-10-21 21:41:06 -0500

    Create e.java

---
+++ Resueltos/Yeison Niño/211020 Toy malito/e.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class Main {
+    
+    
+    public static void main(String[] args) {
+        Scanner tc = new Scanner (System.in);
+        int n = tc.nextInt();
+        System.out.println( n%2==0?"Bob":"Alice");
+        
+    }
+}
