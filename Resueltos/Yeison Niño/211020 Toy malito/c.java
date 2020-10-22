15c41f2ff7b00ff2d40b84e634fc467382f5678b
Yeison Niño <44381397+Morktem@users.noreply.github.com>
2020-10-21 21:40:16 -0500

    Create c.java

---
+++ Resueltos/Yeison Niño/211020 Toy malito/c.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class Main {
+    
+    
+    public static void main(String[] args) {
+        Scanner tc = new Scanner (System.in);
+        int n = tc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i+" Abracadabra");
+        }
+        
+        
+    }
+}
