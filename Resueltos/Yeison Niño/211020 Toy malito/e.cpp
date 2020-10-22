0869cbe152ce5b329b21ffcdff4d1161b41dde90
Yeison Niño <44381397+Morktem@users.noreply.github.com>
2020-10-21 21:41:24 -0500

    Create e.cpp

---
+++ Resueltos/Yeison Niño/211020 Toy malito/e.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int m = n%2;
+    if (m==0) {
+        cout<<"Bob"<<endl;
+    } else {
+        cout<<"Alice"<<endl;
+    }
+}
