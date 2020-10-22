7fdeb9dade06b2613abb6a09732dfd21cafc42aa
juliansilvit <62625597+juliansilvit@users.noreply.github.com>
2020-10-22 15:54:48 -0500

    Create Willcard.java

---
+++ Codigos estudiantes por lenguaje/Java/Julian Silva/Strings/Willcard.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+public class ejercicios {
+//Función que coteja str con el patron wildcard
+
+	static boolean busquedastring(String str, String pattern, int x, int y) {
+// Patron vacio colo puede coincidir
+// con string vacio 
+		if (y == 0) {
+			return (x == 0);
+		}
+
+// Tabla de busqueda para almacenar resultados
+// de subproblemás
+		boolean[][] verificacion = new boolean[x + 1][y + 1];
+//Inicializa la tabla en falso 
+		for (int i = 0; i < x + 1; i++) {
+			Arrays.fill(verificacion[i], false);
+		}
+		verificacion[0][0] = true;
+//Solo '*' puede coincidri con string vacio 
+		for (int j = 1; j <= y; j++) {
+			if (pattern.charAt(j - 1) == '*') {
+				verificacion[0][j] = verificacion[0][j - 1];
+			}
+		}
+// Llena la tabla
+		for (int i = 1; i <= x; i++) {
+			for (int j = 1; j <= y; j++) {
+				/*
+				 * Dos casos que podemos ver de '*' a) Ignoramos '*' y pasamos al siguiente
+				 * caracter en el patrón b) '*' coincide con un iesimo caracter en una entrada
+				 */
+
+				if (pattern.charAt(j - 1) == '*') {
+					verificacion[i][j] = verificacion[i][j - 1] || verificacion[i - 1][j];
+				} /*
+					 * Actuales caracteres son considerados como coincidentes en dos casos a) actual
+					 * caracter de patron es '?' b) caracteres actuales coinciden
+					 */ else if (pattern.charAt(j - 1) == '?' || str.charAt(i - 1) == pattern.charAt(j - 1)) {
+					verificacion[i][j] = verificacion[i - 1][j - 1];
+				} // Si el caracter no coincide
+				else {
+					verificacion[i][j] = false;
+				}
+			}
+		}
+		return verificacion[x][y];
+	}
+
+	public static void main(String args[]) {
+		String str = "baaabab";
+		String pattern = "*****ba*****ab";
+//Casos de prueba
+// String pattern = "ba*****ab";
+
+// String pattern = "ba*ab";
+// String pattern = "a*ab";
+// String pattern = "a*****ab";
+// String pattern = "*a*****ab";
+// String pattern = "ba*ab****";
+// String pattern = "****";
+// String pattern = "*";
+// String pattern = "aa?ab";
+// String pattern = "b*b";
+// String pattern = "a*a";
+// String pattern = "baaabab";
+// String pattern = "?baaabab";
+// String pattern = "*baaaba*";
+
+		if (busquedastring(str, pattern, str.length(), pattern.length())) {
+			System.out.println("Si");
+		} else {
+			System.out.println("No");
+		}
+	}
+}
