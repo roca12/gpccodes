2bf94b5fe4a1154583216290e8c6892ed14a781f
BryanttV <64033729+BryanttV@users.noreply.github.com>
2020-10-23 18:08:25 -0500

    Update finiteAutomata.py

--- Codigos estudiantes por lenguaje/PY/Bryann Valderrama/Strings/finiteAutomata.py
+++ Codigos estudiantes por lenguaje/PY/Bryann Valderrama/Strings/finiteAutomata.py
@@ -1,42 +1,54 @@
+'''Finite Automata
+
+   - Algoritmo de Busqueda de Patrones
+   - Se procesa el patron y se crea una matriz
+   - Complejidad Tiempo: O(len(pat) - 3 * NO_OF_CHARS)
+'''
+
 from sys import stdin, stdout
+rl = stdin.readline
+wr = stdout.write
+
 NO_OF_CHARS = 256
 
 
 def getNextState(pat, M, state, x):
+
     if state < M and x == ord(pat[state]):
         return state + 1
-    i = int(0)
+
+    i = 0
     for ns in range(state, 0, -1):
         if ord(pat[ns-1]) == x:
             for i in range(ns-1):
                 if pat[i] != pat[state-ns+1+i]:
                     break
                 i += 1
-            if i == ns-1:
+            if i == ns - 1:
                 return ns
+
     return 0
 
 
 def computeTF(pat, M, TF):
-    global NO_OF_CHARS
     for state in range(M+1):
         for x in range(NO_OF_CHARS):
             TF[state][x] = getNextState(pat, M, state, x)
 
 
-def finiteAutomata(pat, txt):
-    global NO_OF_CHARS
+def finiteAutomataSearch(pat, txt):
+
     M = len(pat)
     N = len(txt)
-    TF = [[None for x in range(NO_OF_CHARS)] for x in range(M+1)]
+    TF = [[0 for x in range(NO_OF_CHARS)] for x in range(M+1)]
     computeTF(pat, M, TF)
-    state = int(0)
+    state = 0
     for i in range(N):
         state = TF[state][ord(txt[i])]
         if state == M:
-            stdout.write(f'Patron encontrado en indice {i-M+1} - {i}\n')
+            wr(f'"{"".join(pat)}" encontrado en indices ({i-M+1} - {i})\n')
 
 
-txt = [x for x in 'looloolo']
-pat = [x for x in 'olo']
-finiteAutomata(pat, txt)
+txt = 'AABAACAADAABAABA'
+pat = 'AABA'
+finiteAutomataSearch(pat, txt)
