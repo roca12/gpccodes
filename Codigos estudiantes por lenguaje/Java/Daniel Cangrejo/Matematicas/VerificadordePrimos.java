fa9d6bf64fc5491a8f9c70cf25547de1d9f98052
danielcangrejo10 <70976456+danielcangrejo10@users.noreply.github.com>
2020-10-26 15:50:54 -0500

    Create VerificadordePrimos.java

---
+++ Codigos estudiantes por lenguaje/Java/Daniel Cangrejo/Matematicas/VerificadordePrimos.java
@@ -0,0 +1,31 @@
+import java.io.PrintWriter;
+import java.util.*;
+
+//Verificador de primos
+public class Main {
+	
+	static boolean isPrime(int x) {
+		if(x < 2) {
+			return false;
+		}
+		if(x == 2) {
+			return true;
+		}
+		for (int i = 2; i*i <= x; i++) {
+			if(x % i == 0) {
+				return false;
+			}
+		}
+		return true;	
+	}
+	
+	public static void main(String[] args) {
+		int n = 53;
+		if(isPrime(n)) {
+			System.out.println("Es primo");
+		}else {
+			System.out.println("No es primo");
+		}
+		
+	}
+}
