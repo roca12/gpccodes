9b0836a0a4f0521b75c67554c707fc6e83c9efa1
danielcangrejo10 <70976456+danielcangrejo10@users.noreply.github.com>
2020-10-26 14:30:21 -0500

    Create MCD_&_MCM.java

---
+++ Codigos estudiantes por lenguaje/Java/Daniel Cangrejo/Matematicas/MCD_&_MCM.java
@@ -0,0 +1,23 @@
+import java.io.PrintWriter;
+import java.util.*;
+
+
+public class Main {
+	//MAXIMO COMUN DIVISOR
+	static int gcd(int a, int b) {
+		return b==0?a:gcd(b, a%b);
+	} 
+	
+	//MINIMO COMÚN MULTIPLO
+	static int lcm(int a, int b) {
+		return a*(b/gcd(a,b));
+	}
+	
+	public static void main(String[] args) {
+		int a = 80;
+		int b = 30;
+		System.out.println("GCD: " + gcd(a, b));
+		System.out.println("GCD: " + lcm(a, b));
+	
+	}
+}
