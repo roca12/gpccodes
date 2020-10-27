23073d5401540b3ecd83d92b5e485ac525d9fad9
BryanttV <64033729+BryanttV@users.noreply.github.com>
2020-10-26 23:09:44 -0500

    Create LCM_Array.py

---
+++ Codigos estudiantes por lenguaje/PY/Bryann Valderrama/Matemática/LCM_Array.py
@@ -0,0 +1,37 @@
+'''Minimo Comun Multiplo de N numeros
+'''
+
+from sys import stdout
+wr = stdout.write
+
+
+def LCM_array(arr):
+    ans = 1
+    divisor = 2
+
+    while True:
+        counter = 0
+        divisible = False
+
+        for i in range(len(arr)):
+            if arr[0] == 0:
+                return 0
+            elif arr[i] < 0:
+                arr[i] = abs(arr[i])
+            if arr[i] == 1:
+                counter += 1
+            if arr[i] % divisor == 0:
+                divisible = True
+                arr[i] //= divisor
+
+        if divisible:
+            ans = ans * divisor
+        else:
+            divisor += 1
+
+        if counter == len(arr):
+            return ans
+
+
+arr = [3 ,2 ,7 ,23]
+wr(f'{LCM_array(arr)}\n')
