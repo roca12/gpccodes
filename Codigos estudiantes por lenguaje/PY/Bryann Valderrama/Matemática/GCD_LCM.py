93287e3d593b241c3616b0ba3eb3e7cbeeb3129a
BryanttV <64033729+BryanttV@users.noreply.github.com>
2020-10-26 23:07:45 -0500

    Update GCD_LCM.py

--- Codigos estudiantes por lenguaje/PY/Bryann Valderrama/Matemática/GCD_LCM.py
+++ Codigos estudiantes por lenguaje/PY/Bryann Valderrama/Matemática/GCD_LCM.py
@@ -1,4 +1,14 @@
+'''
+Maximo Comun Divisor - Greatest Common Factor (GCD)
+   - Mayor numero entero que divide a 2 numeros sin dejar residuo 
+
+Minimo Comun Multiplo - Least Common Multiple (LCM)
+   - Menor multiplo comun de 2 numeros
+'''
+
 from sys import stdout
+from math import gcd, lcm
+wr = stdout.write
 
 
 def GCD(a, b):
@@ -9,7 +19,10 @@ def LCM(a, b):
     return a * (b // GCD(a, b))
 
 
-a = 8
-b = 53
-stdout.write(f'GCD de a y b = {GCD(a,b)}\n')
-stdout.write(f'LCM de a y b = {LCM(a,b)}')
+a = 60
+b = 70
+wr(f'GCD: {GCD(a, b)}\n')
+wr(f'LCM: {LCM(a, b)}\n')
+
+wr(f'{gcd(a, b)}\n')
+wr(f'{lcm(a, b)}\n')
