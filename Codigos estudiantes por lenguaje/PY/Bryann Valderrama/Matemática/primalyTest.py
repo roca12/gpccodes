393655ae6890ba168bc2a790d8a0c7a070dde7c8
BryanttV <64033729+BryanttV@users.noreply.github.com>
2020-10-26 23:10:33 -0500

    Update primalyTest.py

--- Codigos estudiantes por lenguaje/PY/Bryann Valderrama/Matemática/primalyTest.py
+++ Codigos estudiantes por lenguaje/PY/Bryann Valderrama/Matemática/primalyTest.py
@@ -1,21 +1,30 @@
+'''Primaly Test
+
+   - Se saca la raiz cuadrada entera de N
+   - Se realiza N.mod(i) desde 2 hasta la raiz 
+   - Si algun i es divisible, No es primo.
+   - Si ningun i es divisible, Si es Primo.
+'''
+
 from sys import stdout
+wr = stdout.write
 
 
 def isPrime(x):
+
     if x < 2:
         return False
     if x == 2:
         return True
+
     i = 2
     while i * i <= x:
         if x % i == 0:
             return False
         i += 1
+
     return True
 
 
-n = 14
-if isPrime(n):
-    stdout.write('Es Primo')
-else:
-    stdout.write('No es Primo')
+n = 523
+wr('Es primo') if isPrime(n) else wr('No es Primo')
