876f3317f2ae7aaef539e443e5663e2bad96d7bc
PaulaRios14 <71901320+PaulaRios14@users.noreply.github.com>
2020-10-27 11:55:57 -0500

    Create GCDyLCD.java

---
+++ Codigos estudiantes por lenguaje/Java/Paula Rios/matematicas/GCDyLCD.java
@@ -0,0 +1,14 @@
+public class Main {
+static int gcd(int a, int b){
+    return b==0?a:gcd(b,a%b);
+}
+static int lcm(int a, int b){
+    return a*(b/gcd(a, b));
+}
+   
+    public static void main(String[] args) {
+        int a=60, b=70;
+        System.out.println("GCD: "+gcd(a, b));
+        System.out.println("LCM: "+ lcm(a, b));
+    }
+}
