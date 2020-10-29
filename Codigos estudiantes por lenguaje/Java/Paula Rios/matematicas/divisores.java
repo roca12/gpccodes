0c80e65adc96de79e7ae5a36ceb22a1d5752e7c1
PaulaRios14 <71901320+PaulaRios14@users.noreply.github.com>
2020-10-29 12:54:45 -0500

    Create divisores.java

---
+++ Codigos estudiantes por lenguaje/Java/Paula Rios/matematicas/divisores.java
@@ -0,0 +1,21 @@
+//calcula la cantidad de divisores de un numero
+public class Main {
+    static int divisors(int x){
+        int nDIV=1;
+        for (int i = 2; i*i <=x; i++) {
+           int cnt=0;
+            while(x%i==0){
+                cnt++;
+                x/=i;
+            }
+            nDIV*=cnt+1;
+        }
+        if(x>1){
+        nDIV*=2;
+    }
+        return nDIV;
+    }
+    public static void main(String[] args) {
+        System.out.println(divisors(100));
+    }
+}
