4f72f65fd0829875a6a6843c8cbe0d1243c9a260
danielcangrejo10 <70976456+danielcangrejo10@users.noreply.github.com>
2020-10-29 17:50:34 -0500

    Create FibN_Y_UltimosDigdeFibN.py

---
+++ Codigos estudiantes por lenguaje/PY/Daniel Cangrejo/Matématicas/FibN_Y_UltimosDigdeFibN.py
@@ -0,0 +1,39 @@
+from sys import stdout
+wr = stdout.write
+
+
+def multiply(F, M):
+    x = F[0][0] * M[0][0] + F[0][1] * M[1][0]
+    y = F[0][0] * M[0][1] + F[0][1] * M[1][1]
+    z = F[1][0] * M[0][0] + F[1][1] * M[1][0]
+    w = F[1][0] * M[0][1] + F[1][1] * M[1][1]
+
+    F[0][0] = x
+    F[0][1] = y
+    F[1][0] = z
+    F[1][1] = w
+
+
+def power(F, n):
+    if n == 0 or n == 1:
+        return
+    M = [[1, 1], [1, 0]]
+    power(F, n//2)
+    multiply(F, F)
+    if n % 2 != 0:
+        multiply(F, M)
+
+
+def fib(n):
+    F = [[1, 1], [1, 0]]
+    if n == 0:
+        return 0
+    power(F, n - 1)
+    return F[0][0]
+
+
+def findLastDigit(n):
+    return fib(n) % 10
+
+
+wr(f'{fib(20)} : {findLastDigit(20)}')
