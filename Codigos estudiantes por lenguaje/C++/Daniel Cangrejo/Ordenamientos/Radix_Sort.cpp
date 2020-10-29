99ed94e46535dcf2b23d679de55cfcc56623a0cb
danielcangrejo10 <70976456+danielcangrejo10@users.noreply.github.com>
2020-10-29 17:38:51 -0500

    Create Radix_Sort.cpp

---
+++ Codigos estudiantes por lenguaje/C++/Daniel Cangrejo/Ordenamientos/Radix_Sort.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+using namespace std;
+int getMax(int arr[],int n){
+	int mx=arr[0];
+	for(int i=1;i<n;++i){
+		if(arr[i]>mx){
+			mx=arr[i];
+		}
+	}
+	return mx;
+}
+void countSort(int arr[],int n,int exp){
+	int output[n];
+	int count[10];
+	memset(count,0,sizeof(count));
+	for(int i=0;i<n;++i){
+		count[(arr[i]/exp)%10]++;
+	}
+	for (int i=0;i<10;++i){
+		count[i]+=count[i-1];
+	}
+	for(int i=n-1;i>=0;--i){
+		output[count[(arr[i]/exp)%10]-1]=arr[i];
+		count[(arr[i]/exp)%10]--;
+	}
+	for(int i=0;i<n;++i){
+		arr[i]=output[i];
+	}
+}
+void radixSort(int arr[],int n){
+		int m=getMax(arr,n);
+	for(int exp=1;m/exp >0;exp*=10){
+		countSort(arr,n,exp);
+	}
+}
+void printArray(int arr[],int n){
+	for(int i=0;i<n;++i){
+		printf("%d ",arr[i]);
+	}
+	cout<<"\n";
+}
+int main(int argc, char const *argv[]) {
+	int arr[]={9,78,6,5,32,1,1,4,5,45};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	radixSort(arr,n);
+	printArray(arr,n);
+	return 0;
+}
