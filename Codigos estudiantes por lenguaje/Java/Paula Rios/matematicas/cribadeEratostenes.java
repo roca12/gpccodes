78ec2b203e44182220a22b51d088a8f10b0f16a6
PaulaRios14 <71901320+PaulaRios14@users.noreply.github.com>
2020-10-29 14:39:18 -0500

    Create cribadeEratostenes.java

---
+++ Codigos estudiantes por lenguaje/Java/Paula Rios/matematicas/cribadeEratostenes.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+public class Main {
+    static boolean[] sieveEra(int N){
+        boolean[] prime=new boolean[N+1];
+        Arrays.fill(prime,true);
+        prime[0]=prime[1]=false;
+        for (int p =2; p*p <=N; p++) {
+            if(prime[p]){
+                for (int i = p*p; i <=N; i+=p) {
+                    prime[i]=false;
+                }
+            }
+        }
+        return prime;
+  
+    }
+    public static void main(String[] args) {
+        boolean []res= sieveEra(200);
+        for (int i = 0; i <res.length; i++) {
+            System.out.println(i+" ->"+res[i]);
+        }
+    }
+}
