8cc571f45b1404b7969cef97deac6fa7dcd65f6b
PaulaRios14 <71901320+PaulaRios14@users.noreply.github.com>
2020-10-29 18:40:26 -0500

    Create binarysearch.cpp

---
+++ Codigos estudiantes por lenguaje/C++/Paula Rios/busquedas/binarysearch.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+using namespace std;
+int binarySearch(int arr[],int left,int right,int x){
+    if(right>=left){
+        int mid=left+(right-left)/2;
+        if(arr[mid]==x){
+            return mid;
+        }
+        if(arr[mid]>x){
+            return binarySearch(arr,left,mid-1,x);
+        }
+        return binarySearch(arr,mid+1,right,x);
+    }
+    return -1;
+}
+int main(){
+    int arr[]={2,3,4,10,40};
+    int n=sizeof (arr)/sizeof (arr[0]);
+    sort(arr,arr+n);
+    int x=40;
+    int result=binarySearch(arr,0,n-1,x);
+    if(result==-1){
+        cout<<"Dato NO encontrado";
+    }else{
+        cout<<"El dato esta en la posicion "<<result;
+    }
+}
