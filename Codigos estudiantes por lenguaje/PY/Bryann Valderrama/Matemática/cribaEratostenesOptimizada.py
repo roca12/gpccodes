ac119d1fd7769f0d38d909015965199f3a5cc525
BryanttV <64033729+BryanttV@users.noreply.github.com>
2020-10-29 12:06:29 -0500

    Update cribaEratostenesOptimizada.py

--- Codigos estudiantes por lenguaje/PY/Bryann Valderrama/Matemática/cribaEratostenesOptimizada.py
+++ Codigos estudiantes por lenguaje/PY/Bryann Valderrama/Matemática/cribaEratostenesOptimizada.py
@@ -1,42 +1,43 @@
+'''Criba Eratostenes Optimizada
+
+   - Similar a la Criba de Eratostenes convencional
+   - Para cada numero i, entre 2 y N, si i es primo, se almacena en el array prime
+   - Para cada número primo j menor o igual que el menor factor primo p de i,
+     marque todos los números j * p como no primo. Marque el factor primo más pequeño de j * p como j
+   - Complejidad Tiempo: O(n)
+'''
+
 from sys import stdout
 wr = stdout.write
 
+MAX = 100001  # Limite de Busqueda
+isPrime = [True] * MAX
+prime = []  # Almacena Numeros Primos
+SPF = [2] * MAX  # Almacena Factores Primos
 
-MAX_SIZE = 1000001
-isPrime = [True for x in range(MAX_SIZE)]
-prime = []
-SPF = [0 for x in range(MAX_SIZE)]
 
+def manipulatedSieve(N):
 
-def manipuledSieve(n):
-    global isPrime
-    global prime
-    global SPF
-    isPrime[0] = False
-    isPrime[1] = False
-    for i in range(2, n):
-        if(isPrime[i]):
+    isPrime[0] = isPrime[1] = False
+    for i in range(2, N):
+        if isPrime[i]:
             prime.append(i)
             SPF[i] = i
         j = 0
-        while j < len(prime) and i * prime[j] < n and prime[j] <= SPF[i]:
-            isPrime[i * prime[j]] = False
-            SPF[i * prime[j]] = prime[j]
+        while j < len(prime) and i * prime[j] < N and prime[j] <= SPF[i]:
+            isPrime[i*prime[j]] = False
+            SPF[i*prime[j]] = prime[j]
             j += 1
 
 
-n = 100
-for i in range(MAX_SIZE):
-    SPF.append(2)
+N = 30
+manipulatedSieve(N)
 
-manipuledSieve(n)
-
-# Calcula e Imprime los primeros 100 numeros primos
-for i in range(len(prime)):
-    wr(f'{prime[i]} ')
-
-wr("\n")
-
-# Calcula e Imprime el factor primo mas pequeño de los primeros n numeros
-for i in range(2, n):
-    wr(f"{i} : {SPF[i]} \n")
+for i in prime[:N]:
+    wr(f'{i} ')
+wr('\n')
+for i in isPrime[:N]:
+    wr(f'{i} ')
+wr('\n')
+for i in SPF[:N]:
+    wr(f'{i} ')
