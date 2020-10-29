ecbfa7b6e1079be4c11263f90b84239514aa088f
BryanttV <64033729+BryanttV@users.noreply.github.com>
2020-10-29 12:05:34 -0500

    Update cribaEratostenes.py

--- Codigos estudiantes por lenguaje/PY/Bryann Valderrama/Matemática/cribaEratostenes.py
+++ Codigos estudiantes por lenguaje/PY/Bryann Valderrama/Matemática/cribaEratostenes.py
@@ -1,27 +1,34 @@
+''' Criba de Eratostenes 
+
+    - Hallar todos los numeros primos menores que un numero N
+    - Se forma una tabla con todos los numeros enteros comprendidos entre 2 y n
+    - Se comienza desde 2, y se tachan todos sus multiplos
+    - Comenzando de nuevo, al encontrar un numero entero no tachado, ese numero es primo
+    - Se procede a tachar todos los multiplos de ese numero primo, y asi sucesivamente
+    - Complejidad Tiempo: O(n log (log n))
+'''
+
 from sys import stdout
 wr = stdout.write
 
 
-def sieveErathostenes(n):
-    prime = [True for x in range(n+1)]
-    prime[0] = False
-    prime[1] = False
+def sieveEra(N):
+
+    prime = [True for x in range(N+1)]
+    prime[0] = prime[1] = False
+
     p = 2
-    while p*p <= n:
+    while p * p <= N:
         if prime[p]:
-            i = p*p
-            while i <= n:
+            i = p * p
+            while i <= N:
                 prime[i] = False
                 i += p
         p += 1
+
     return prime
 
 
-res = sieveErathostenes(1000)
-
-# Calcula e Imprime los primeros n numeros
-# True is Prime
-# False is not Prime
-
-for i in range(len(res)):
-    wr(f'{i} : {res[i]}\n')
+ans = sieveEra(99)
+for i, j in enumerate(ans, 99):
+    wr(f'{i} : {j}\n')
