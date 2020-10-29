8d62da98101e04ada865bed1a8ce4297212f72c2
danielcangrejo10 <70976456+danielcangrejo10@users.noreply.github.com>
2020-10-29 14:04:54 -0500

    Create BackToHighSchoolPhysics.java

---
+++ Resueltos/Daniel Cangrejo/Java/BackToHighSchoolPhysics.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()) {
+		int v = sc.nextInt();  int t = sc.nextInt();
+		int res = ((v*t)*2);
+		System.out.println(res);
+		}
+
+	}
+
+}
