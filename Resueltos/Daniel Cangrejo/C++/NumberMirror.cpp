a3de618ab22b333d39e3e0e255747771e56c91e5
danielcangrejo10 <70976456+danielcangrejo10@users.noreply.github.com>
2020-10-29 13:27:51 -0500

    Create NumberMirror.cpp

---
+++ Resueltos/Daniel Cangrejo/C++/NumberMirror.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<cstdlib>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	
+	int numero;
+	cin>>numero;	
+	cout<<numero;
+	
+	
+	
+	return 0;
+}
