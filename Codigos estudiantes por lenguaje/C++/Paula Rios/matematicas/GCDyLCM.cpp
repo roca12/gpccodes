07a175340488b7bb17ec1cb6f892c544b7754c0b
PaulaRios14 <71901320+PaulaRios14@users.noreply.github.com>
2020-10-29 18:03:47 -0500

    Create GCDyLCM.cpp

---
+++ Codigos estudiantes por lenguaje/C++/Paula Rios/matematicas/GCDyLCM.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<cstdlib>
+using namespace std;
+int gcd(int a,int b){
+    return (b==0?a:gcd(b,a%b));
+}
+int lcm(int a,int b){
+    return (a*(b/gcd(a,b)));
+}
+int main() {
+    int a=60;
+    int b=70;
+    cout<<"GCD : "<<(gcd(a,b))<<endl;
+    cout<<"LCM : "<<(lcm(a,b))<<endl;
+    return 0;
+}
