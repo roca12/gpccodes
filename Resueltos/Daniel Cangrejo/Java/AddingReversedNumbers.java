70c26a2d75ade205f1bff48dd4c17f299b559753
danielcangrejo10 <70976456+danielcangrejo10@users.noreply.github.com>
2020-10-29 14:25:29 -0500

    Create AddingReversedNumbers.java

---
+++ Resueltos/Daniel Cangrejo/Java/AddingReversedNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int re = sc.nextInt();
+    for (int i = 0; i < re; i++) {
+        long numero1 = sc.nextLong();
+        long numer02 = sc.nextLong();
+        long  resultado = (reverse(numero1)+reverse(numer02));
+        System.out.println(reverse(resultado));    
+    }    
+  }
+    
+    static long reverse(long numero) {
+        long resultado = 0;
+        while(numero > 0) {
+            resultado = resultado*10 + numero%10;
+            numero = numero/10;
+        }
+        return resultado;
+    }
+}
