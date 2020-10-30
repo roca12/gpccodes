001755d15aa74bb5d7780963510c8546f0e6c490
5AresAce6 <70976289+5AresAce6@users.noreply.github.com>
2020-10-30 14:13:46 -0500

    Create (PY)RKSearch.py

---
+++ Codigos estudiantes por lenguaje/PY/Acevedo_Sebastian/Patterns_and_Strings/(PY)RKSearch.py
@@ -0,0 +1,43 @@
+'''Rabin-Karp
+   - Busca un SubString dentro de un String.
+   - Busca Hashes similares y luego compara los caracteres individuales.
+   - Complejidad Tiempo: 
+        - Promedio y Mejor Caso: O(n+m).
+        - Peor Caso: O(n*m).
+'''
+
+from sys import stdin, stdout
+wr = stdout.write
+
+D = 256  # Caracteres del ASCII
+
+
+def RKsearch(pat, txt, q):
+    global D
+    M = len(pat)
+    N = len(txt)
+    j = 0  # i itera en el texto, j itera en el patron
+    h, p, t = 1, 0, 0  # Hashes
+
+    for i in range(M-1):
+        h = (h*D) % q
+    for i in range(M):
+        p = (D * p + ord(pat[i])) % q
+        t = (D * t + ord(txt[i])) % q
+    for i in range(N-M+1):
+        if p == t:
+            for j in range(M):
+                if txt[i+j] != pat[j]:
+                    break
+            j += 1
+            if j == M:
+                wr(f'"{txt[i:i+M]}" encontrado en el indice {i} - {i+M-1}\n')
+
+        if i < N-M:
+            t = (D * (t - ord(txt[i]) * h) + ord(txt[i+M])) % q
+            if t < 0:
+                t += q
+
+txt = 'ANITALAVALATINA'
+pat = 'AL'
+RKsearch(pat, txt, 17)
