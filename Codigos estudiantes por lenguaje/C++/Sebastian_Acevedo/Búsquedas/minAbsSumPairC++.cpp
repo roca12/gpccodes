e1d1e0f19cebaaa0520f38b502a843dbdada91d1
5AresAce6 <70976289+5AresAce6@users.noreply.github.com>
2020-10-30 12:39:58 -0500

    Create minAbsSumPairC++.cpp

---
+++ Codigos estudiantes por lenguaje/C++/Sebastian_Acevedo/Búsquedas/minAbsSumPairC++.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+using namespace std;
+void minAbsoluteSumPair(int arr[],int n){
+    int left,right,min_sum,sum,minl,minr;
+    if(n==2){
+        printf("invalido");
+        return;
+    }
+    minl=0;
+    minr=1;
+    min_sum=arr[0]+arr[1];
+    for(left=0; left<n-1;left++){
+        for(right = left+1;right<n;right++){
+            sum=arr[left]+arr[right];
+            if(abs(min_sum)>abs(sum)){
+                min_sum=sum;
+                minl=left;
+                minr=right;
+            }
+        }
+    }
+    printf("la suma minima tiene los valores de %d %d : %d",arr[minl],arr[minr],min_sum);
+}
+int main() {
+    int arr[]={1,60,-10,70,-80,85};
+    int n=sizeof (arr)/sizeof (arr[0]);
+    minAbsoluteSumPair(arr,n);
+}
