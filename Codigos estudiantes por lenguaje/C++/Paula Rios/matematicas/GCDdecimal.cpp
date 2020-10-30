beebdf5b0f9845297769c74bf10a7bad047369ca
PaulaRios14 <71901320+PaulaRios14@users.noreply.github.com>
2020-10-29 19:07:46 -0500

    Create GCDdecimal.cpp

---
+++ Codigos estudiantes por lenguaje/C++/Paula Rios/matematicas/GCDdecimal.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<cstdlib>
+using namespace std;
+double gcd(double a,double b){
+    if(a<b){
+        return gcd(b,a);
+    }
+    if(abs(b)<0.001){
+        return a;
+    }
+    return (gcd(b,a-floor(a/b)*b));
+}
+
+int main() {
+    double a=1.20;
+    double b=22.5;
+    printf("%1.1f\n",gcd(a,b));
+    return 0;
+}
