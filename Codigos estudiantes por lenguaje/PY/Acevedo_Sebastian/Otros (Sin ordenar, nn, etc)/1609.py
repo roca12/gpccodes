d47cd048eb3e5933059c4f9d53de9454c29fe202
5AresAce6 <70976289+5AresAce6@users.noreply.github.com>
2020-10-30 14:23:49 -0500

    Create 1609.py

---
+++ Codigos estudiantes por lenguaje/PY/Acevedo_Sebastian/Otros (Sin ordenar, nn, etc)/1609.py
@@ -0,0 +1,27 @@
+
+from sys import stdin, stdout
+rl = stdin.readline
+wr = stdout.write
+
+
+num = 69.4205
+wr(f'{round(num,2)}') # 2 decimales
+
+"""
+from sys import stdin, stdout
+
+n = float(stdin.readline())
+
+stdout.write(f"{n:.4f}\n")
+
+stdout.write(f"{round(n,4)}")
+
+
+
+from sys import stdin, stdout
+
+n = float(stdin.readline())
+
+stdout.write(f"%e"%n)
+
+"""
