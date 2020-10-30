df9ee5349f1cefb19dd36ce695fc6858e991f940
5AresAce6 <70976289+5AresAce6@users.noreply.github.com>
2020-10-30 15:28:28 -0500

    Create Vector.py

---
+++ Codigos estudiantes por lenguaje/PY/Acevedo_Sebastian/Otros (Sin ordenar, nn, etc)/Vector.py
@@ -0,0 +1,11 @@
+"""
+for i in range (int(input())):
+    cadena=input()
+    resultado=int(cadena[0]+int(cadena[-1]))
+    print(resultado)
+"""
+lista=[]
+for i in range(10):
+    lista.append(i*i)
+for i in lista:
+    print(i)
