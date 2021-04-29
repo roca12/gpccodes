703578f8026c90b808a57280f332832e7c4c24ee
Edwin Andres Villarraga Ardila <62625858+edanv1401@users.noreply.github.com>
2021-04-29 07:44:30 -0500

    Create PrefixSum(Version Edwin).cpp

---
+++ Codigos estudiantes por lenguaje/C++/Edwin Villarraga/Nuevos Aprendidos/PrefixSum(Version Edwin).cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+ll prefixSum(int i,int j,ll arr[]){
+    ll sum_i=0,sum_j=0;
+    return arr[j]-arr[i-1];
+}
+ll op(ll a,ll b){
+    return a+b;
+}
+void sumArr(ll temp[],ll values[],int n,int index){
+    if(index==n){
+        return;
+    }
+    if(index==0){
+        sumArr(temp,values,n,index+1);
+    }
+    temp[index]=op(values[index-1],temp[index-1]);
+    sumArr(temp,values,n,index+1);
+}
+int main(){
+    ll arr[]={3,4,65,6,3,2,34,6,8,9,7,5,1,6};
+    int n= sizeof(arr)/sizeof(arr[0]);
+    ll temp[n+1];
+    memset(temp,0,sizeof(temp));
+    sumArr(temp,arr,n+1,1);
+    cout<<endl;
+    for_each(temp,temp+n+1,[&](ll a){
+             cout<<a<<" ";
+             });
+             cout<<endl;
+    cout<<prefixSum(7,8,temp)<<endl;
+    return 0;
+}
