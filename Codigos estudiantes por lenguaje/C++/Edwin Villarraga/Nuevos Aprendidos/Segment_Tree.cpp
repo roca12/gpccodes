6a27d2f0875fbf16a28df8de9093541debb6bd19
Edwin Andres Villarraga Ardila <62625858+edanv1401@users.noreply.github.com>
2021-04-29 07:55:36 -0500

    Update Segment_Tree.cpp

--- Codigos estudiantes por lenguaje/C++/Edwin Villarraga/Nuevos Aprendidos/Segment_Tree.cpp
+++ Codigos estudiantes por lenguaje/C++/Edwin Villarraga/Nuevos Aprendidos/Segment_Tree.cpp
@@ -1,7 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
-
+void sumArr(ll temp[],ll values[],int n,int index){
+    if(index==n){
+        return;
+    }
+    if(index==0){
+        sumArr(temp,values,n,index+1);
+    }
+    temp[index]=op(values[index-1],temp[index-1]);
+    sumArr(temp,values,n,index+1);
+}
 int main(){
+    ll arr[]={3,2,5,1,3};
+    /*
+             14
+            /  \
+           /    \
+         10      4
+        /  \    / \
+       5    5  1   3
+      / \
+     3   2
+    */
+    int n= sizeof(arr)/sizeof(arr[0]);
+    ll values[n+1];
+    memset(values,0,sizeof(values));
+    sumArr(values,arr,n+1,1);
     return 0;
 }
