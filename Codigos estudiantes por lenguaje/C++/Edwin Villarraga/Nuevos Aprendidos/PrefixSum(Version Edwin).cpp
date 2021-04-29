5fdf5020ccdb3ae0906fb68f90a6c23cc2768458
Edwin Andres Villarraga Ardila <62625858+edanv1401@users.noreply.github.com>
2021-04-29 07:45:47 -0500

    Update PrefixSum(Version Edwin).cpp

--- Codigos estudiantes por lenguaje/C++/Edwin Villarraga/Nuevos Aprendidos/PrefixSum(Version Edwin).cpp
+++ Codigos estudiantes por lenguaje/C++/Edwin Villarraga/Nuevos Aprendidos/PrefixSum(Version Edwin).cpp
@@ -8,6 +8,7 @@ ll prefixSum(int i,int j,ll arr[]){
 ll op(ll a,ll b){
     return a+b;
 }
+//calculo de las sumas adyacentes
 void sumArr(ll temp[],ll values[],int n,int index){
     if(index==n){
         return;
@@ -21,14 +22,17 @@ void sumArr(ll temp[],ll values[],int n,int index){
 int main(){
     ll arr[]={3,4,65,6,3,2,34,6,8,9,7,5,1,6};
     int n= sizeof(arr)/sizeof(arr[0]);
+    //alojamos todas las sumas de forma lineal (arr[current],arr[current+1]);
     ll temp[n+1];
     memset(temp,0,sizeof(temp));
     sumArr(temp,arr,n+1,1);
     cout<<endl;
+    /*
     for_each(temp,temp+n+1,[&](ll a){
              cout<<a<<" ";
              });
              cout<<endl;
+             */
     cout<<prefixSum(7,8,temp)<<endl;
     return 0;
 }
