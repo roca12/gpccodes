b2b25c32e5c8ca1f7f312faf90ebc160a32a7c47
Edwin Andres Villarraga Ardila <62625858+edanv1401@users.noreply.github.com>
2023-01-02 11:47:12 -0500

    Update UkkonenSuffixTree.cpp

--- Por traducir/UkkonenSuffixTree.cpp
+++ Por traducir/UkkonenSuffixTree.cpp
@@ -1,4 +1,3 @@
-
 #include<bits/stdc++.h>
 #include<cstdlib>
 
@@ -9,17 +8,18 @@ class NodeMayor{
         int _end;
         int suffixIndex;
         bool verifyNull = false;
-        map<char, NodeMayor> edgeMap;
-        NodeNodeMayor(){};
+        NodeMayor(){
+            verifyNull = false;
+            start = -1;
+            _end = -1;
+            suffixIndex = -1;
+        };
         NodeMayor(int _start){
             start = _start;
         }
         NodeMayor(int _start, int __end){
             start = _start;
             _end = __end;
-        }
-        string __to_string(string phrase, int leafEnd){
-            return _end == -1? phrase.substr(start, leafEnd+1) : phrase.substr(start, _end +1);
         }
 
         bool operator !=(const NodeMayor &n){
@@ -37,28 +37,84 @@ class NodeMayor{
                     );
         }
 };
-struct Node: NodeMayor{
-    NodeMayor suffixLink;
-    Node(NodeMayor sL, int _start):NodeMayor(_start){
-        suffixLink = sL;
+
+struct _Node: NodeMayor{
+    vector<_Node> suffixLink;
+    map<char, _Node> edgeMap;
+    _Node():NodeMayor(){
+        suffixLink.clear();
+        edgeMap.clear();
     }
-    Node(NodeMayor sL, int _start, int __end):NodeMayor(_start, __end){
-        suffixLink = sL;
+    _Node(int _start):NodeMayor(_start){
+        suffixLink.clear();
+    }
+    _Node(int _start, int __end):NodeMayor(_start, __end){
+        suffixLink.clear();
     }
 };
+
+struct Node:_Node{
+    Node():_Node(){
+        suffixLink = vector<_Node>{};
+        verifyNull = false;
+        start = -1;
+        _end = -1;
+        suffixIndex = -1;
+    }
+    Node(_Node _sl, int _start):_Node(_start){
+        suffixLink = vector<_Node>{_sl};
+    }
+    Node(_Node _sl, int _start, int __end):_Node(_start, __end){
+        suffixLink = vector<_Node>{_sl};
+    }
+    string __to_string(string phrase, int leafEnd){
+        return _end == -1? phrase.substr(start, leafEnd+1) : phrase.substr(start, _end +1);
+    }
+};
+
+_Node convertTo(Node n){
+    _Node newNode;
+    newNode.edgeMap = n.edgeMap;
+    newNode.start = n.start;
+    newNode._end = n._end;
+    newNode.suffixIndex = n.suffixIndex;
+    newNode.suffixLink = n.suffixLink;
+    newNode.verifyNull = n.verifyNull;
+    return newNode;
+}
+
+Node convertToNode(_Node n){
+    Node newNode;
+    newNode.edgeMap = n.edgeMap;
+    newNode.start = n.start;
+    newNode._end = n._end;
+    newNode.suffixIndex = n.suffixIndex;
+    newNode.suffixLink = n.suffixLink;
+    newNode.verifyNull = n.verifyNull;
+    return newNode;
+}
 struct Ukkonen{
     bool DETAILED = true;
     char UNIQUE = '$';
 
     // private
     string phrase;
-    Node *root;
+    Node root;
     int leafEnd;
     Node internalNode;
     Node activeNode;
     int activeEdge;
     int activeLength;
     int remainingSuffixCount;
+
+    Ukkonen(){
+        root = Node(root, -1);
+        leafEnd = -1;
+        activeNode = root;
+        activeEdge = -1;
+        activeLength = 0;
+        remainingSuffixCount = 0;
+    }
 
     void printActivePoint() {
         string sb = "ActivePoint is (";
@@ -68,13 +124,13 @@ struct Ukkonen{
             sb+=(phrase.substr(activeNode.start, activeNode.start + 1))+(", ");
         }
         sb+=(phrase.substr(activeEdge, activeEdge + 1)) + (", ");
-        sb+=(activeLength+'$') + (")");
+        sb+=to_string(activeLength) + (")");
         cout<<sb<<endl;
     }
 
 
     int edgeLength(Node n) {
-        return n._end != -1 ? n._end - n.start + 1 : leafEnd - n.start + 1;
+        return (n._end != -1) ? (n._end - n.start + 1) : (leafEnd - n.start + 1);
     }
 
     bool walkedDown(Node n) {
@@ -122,13 +178,18 @@ struct Ukkonen{
                     if (DETAILED) {
                         cout<<"Set up suffixLink from last internal node to activeNode"<<endl;
                     }
-                    internalNode.suffixLink = &activeNode;
+                    internalNode.suffixLink = vector<_Node>{activeNode};
                     internalNode.verifyNull = true;
                 }
 
             } else {
-
-                Node next = activeNode.edgeMap.find(phrase[activeEdge])->second;
+                _Node _next = activeNode.edgeMap.find(phrase[activeEdge])->second;
+                Node next;
+                next.edgeMap = _next.edgeMap;
+                next.start = _next.start;
+                next.suffixIndex = _next.suffixIndex;
+                next.suffixLink = _next.suffixLink;
+                next.verifyNull = _next.verifyNull;
                 if (DETAILED) {
                     cout<<"Trick One: Skip/Count"<<endl;
                 }
@@ -139,8 +200,8 @@ struct Ukkonen{
                     if (DETAILED) {
                         cout<<"Rule Three"<<endl;
                     }
-                    if (!internalNode.verifyNull && &activeNode != root) {
-                        internalNode.suffixLink = &activeNode;
+                    if (!internalNode.verifyNull && activeNode != root) {
+                        internalNode.suffixLink = vector<_Node>{activeNode};
                         internalNode.verifyNull = true;
                     }
                     if (DETAILED) {
@@ -161,7 +222,8 @@ struct Ukkonen{
                     int splitEnd = next.start + activeLength - 1;
                     Node split = Node(root, next.start, splitEnd);
                     activeNode.edgeMap.insert({phrase[activeEdge], split});
-                    split.edgeMap.insert({phrase[index], {root, index}});
+                    Node newNode = Node(root, index);
+                    split.edgeMap.insert({phrase[index], convertTo(newNode)});
                     next.start += activeLength;
                     split.edgeMap.insert({phrase[next.start], next});
 
@@ -169,13 +231,13 @@ struct Ukkonen{
                         if (DETAILED) {
                             cout<<"Set up suffixLink from last internal node to this newly created one"<<endl;
                         }
-                        internalNode.suffixLink = &split;
+                        internalNode.suffixLink = vector<_Node>{split};
                     }
                     internalNode = split;
                 }
 
                 remainingSuffixCount--;
-                if (&activeNode == root && activeLength > 0) {
+                if (activeNode == root && activeLength > 0) {
                     if (DETAILED) {
                         cout<<"ER2C1"<<endl;
                     }
@@ -184,11 +246,11 @@ struct Ukkonen{
                     if (DETAILED) {
                         printActivePoint();
                     }
-                } else if (&activeNode != root) { // ER2C2
+                } else if (activeNode != root) { // ER2C2
                     if (DETAILED) {
                         cout<<"ER2C2"<<endl;
                     }
-                    activeNode = *activeNode.suffixLink;
+                    activeNode = convertToNode(activeNode.suffixLink[0]);
                     if (DETAILED) {
                         printActivePoint();
                     }
@@ -201,11 +263,11 @@ struct Ukkonen{
             return;
         }
         if (n.start != -1) {
-            cout<<n;
+            cout<<n.__to_string(phrase, leafEnd);
         }
         bool isLeaf = true;
-        for (Entry<Character, Node> entry : n.edgeMap.entrySet()) {
-            Node node = entry.getValue();
+        for (auto entry : n.edgeMap) {
+            Node node = convertToNode(entry.second);
             // if n is inner node
             if (isLeaf && n.start != -1) {
                 cout<<" "<<n.suffixIndex<<endl;
@@ -225,7 +287,7 @@ struct Ukkonen{
             printActivePoint();
         }
     }
-    void progress(string str){
+    void process(string str){
         if(true){
 
         }
@@ -246,5 +308,6 @@ struct Ukkonen{
 };
 
 int main(){
-    cout<<1<<endl;
+    Ukkonen suffixTree = Ukkonen();
+    suffixTree.process("abcabxabcd$");
 }
